c0f723d2901b47f122ba8e27e296979668753db6
JinHwanKim <ecsimsw@gmail.com>
2021-07-06 22:00:24 +0900

    chore : 초기 프로젝트 세팅 (#21)
    * Update README.md
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * chore: FE 레포 초기화
    
    * chore: back-end 기본 설정
    
    * chore: apply .gitignore
    
    * chore: FE 레포 환경설정
    
    - hello world 를 표시하기 위한 최소한의 패키지 설치 및 환경설정
    
    * chore: FE 레포 환경설정
    
    - storybook, styled-components, eslint, prettier 설정
    
    * chore(fe): 불필요한 webpack target 설정을 삭제한다
    
    - babel-loader가 아닌 ts-loader를 사용하여 browserslist 적용불가하므로 target프로퍼티를 삭제한다
    (target 프로퍼티의 default는 web이다)
    
    * chore: gitignore 환경설정 셋팅 (#14)
    
    * chore: setup gitignore
    
    * chore: 스프링부트 버전 2.4.5로 변경
    
    * chore: 개행 반영
    
    * chore: 개행 반영 추가
    
    * chore: FE용 editorconfig 설정
    
    * chore: gitignore 세팅 미션과 동일하게 진행
    
    Co-authored-by: wannte <49307266+wannte@users.noreply.github.com>
    Co-authored-by: bigsaigon333 <likepeppermint@gmail.com>
    Co-authored-by: Wannte <T.wannte@gmail.com>
    Co-authored-by: knae11 <66905013+knae11@users.noreply.github.com>

---
+++ back-end/src/main/java/admin/Application.java
@@ -0,0 +1,12 @@
+package admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("hello World");
+    }
+}
---
+++ front-end/src/App.styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.h1`
+  font-size: 3rem;
+  color: red;
+`;
