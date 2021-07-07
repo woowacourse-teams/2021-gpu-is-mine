76783f34b844b7e70056cd371ad80e67b3f3d98b
naeunkim <knae11@naver.com>
2021-07-07 10:57:32 +0900

    feat: user 도메인 설계

---
+++ back-end/src/main/java/admin/gpu/domain/User.java
@@ -0,0 +1,16 @@
+package admin.gpu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private UserType userType;
+
+}
