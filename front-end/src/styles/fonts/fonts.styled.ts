2d0c10078786b48802cbcb78d8d34d792fa0632d
bigsaigon333 <likepeppermint@gmail.com>
2021-07-07 20:43:35 +0900

    chore: 폰트 확장자 변경

--- front-end/src/styles/fonts/fonts.styled.ts
+++ front-end/src/styles/fonts/fonts.styled.ts
@@ -1,45 +1,45 @@
 import { css } from "styled-components";
-import NotoSansKR_Thin from "./noto-sans-kr/NotoSansKR-Thin.otf";
-import NotoSansKR_Light from "./noto-sans-kr/NotoSansKR-Light.otf";
-import NotoSansKR_Regular from "./noto-sans-kr/NotoSansKR-Regular.otf";
-import NotoSansKR_Medium from "./noto-sans-kr/NotoSansKR-Medium.otf";
-import NotoSansKR_Bold from "./noto-sans-kr/NotoSansKR-Bold.otf";
-import NotoSansKR_Black from "./noto-sans-kr/NotoSansKR-Black.otf";
+import NotoSansKR_Thin from "./noto-sans-kr/NotoSansKR-Thin.woff2";
+import NotoSansKR_Light from "./noto-sans-kr/NotoSansKR-Light.woff2";
+import NotoSansKR_Regular from "./noto-sans-kr/NotoSansKR-Regular.woff2";
+import NotoSansKR_Medium from "./noto-sans-kr/NotoSansKR-Medium.woff2";
+import NotoSansKR_Bold from "./noto-sans-kr/NotoSansKR-Bold.woff2";
+import NotoSansKR_Black from "./noto-sans-kr/NotoSansKR-Black.woff2";
 
 const notoSansKR = css`
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Thin}) format("opentype");
+    src: url(${NotoSansKR_Thin}) format("woff2");
     font-weight: 100;
   }
 
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Light}) format("opentype");
+    src: url(${NotoSansKR_Light}) format("woff2");
     font-weight: 300;
   }
 
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Regular}) format("opentype");
+    src: url(${NotoSansKR_Regular}) format("woff2");
     font-weight: 400;
   }
 
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Medium}) format("opentype");
+    src: url(${NotoSansKR_Medium}) format("woff2");
     font-weight: 500;
   }
 
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Bold}) format("opentype");
+    src: url(${NotoSansKR_Bold}) format("woff2");
     font-weight: 700;
   }
 
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Black}) format("opentype");
+    src: url(${NotoSansKR_Black}) format("woff2");
     font-weight: 900;
   }
 `;
