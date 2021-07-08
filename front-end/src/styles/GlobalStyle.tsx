9826191ad68ab4e7b6ec97fcca33f577d3ec3595
bigsaigon333 <likepeppermint@gmail.com>
2021-07-08 20:36:26 +0900

    feat: color scheme 정의

--- front-end/src/styles/GlobalStyle.tsx
+++ front-end/src/styles/GlobalStyle.tsx
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 import fonts from "./fonts/fonts.styled";
 import reset from "./reset";
+import colors from "./colors";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
   ${fonts}
+  ${colors}
 
   :root {
     font-family: "Noto Sans KR";
