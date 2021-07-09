78f41bc718dce8346c3ff966fadce4b90d7409e6
JinHwanKim <ecsimsw@gmail.com>
2021-07-10 01:17:27 +0900

    Update auto_script.sh

--- back-end/auto_script.sh
+++ back-end/auto_script.sh
@@ -13,5 +13,5 @@ echo "> new app deploy"
 JAR_NAME=$(ls |grep 'back-end' | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 JAVA=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-nohup $JAVA -jar $JAR_NAME &
+nohup $JAVA -jar $JAR_NAME & >> nohup.out 2>&1 
 sleep 3
