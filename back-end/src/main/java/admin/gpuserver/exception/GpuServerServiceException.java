39ee74674365c14589c709468cd1914cea9dadcd
sjpark-dev <sjpark.dev@gmail.com>
2021-07-10 12:28:46 +0900

    refactor: 예외처리 과정 수정

--- back-end/src/main/java/admin/gpuserver/exception/GpuServerServiceException.java
+++ back-end/src/main/java/admin/gpuserver/exception/GpuServerServiceException.java
@@ -1,9 +1,5 @@
 package admin.gpuserver.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class GpuServerServiceException extends RuntimeException {
 
     public GpuServerServiceException(String message) {
