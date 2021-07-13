3ae99f4e5affecba2ef114c40c913f2493183d97
Jung-ChanYoung <38939015+MyaGya@users.noreply.github.com>
2021-07-13 15:48:08 +0900

    feat: 큐 기본 기능 구현
    컴파일 문제 때문에 data.sql 삭제

---
+++ back-end/src/main/java/queue/Runner.java
@@ -0,0 +1,32 @@
+package queue;
+
+import java.util.concurrent.TimeUnit;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName,
+            "routingKey.1",
+            "Hello from RabbitMQ!");
+
+        rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName,
+            "routingKey.2",
+            "Hello from RabbitMQ!");
+
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    }
+}
