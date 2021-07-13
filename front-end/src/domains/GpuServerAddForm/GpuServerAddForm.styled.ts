ff8e380c21a250c13eaa02fe397ee3043e4e3137
bigsaigon333 <likepeppermint@gmail.com>
2021-07-13 23:36:47 +0900

    feat: GpuServerAddForm 컴포넌트 구현

---
+++ front-end/src/domains/GpuServerAddForm/GpuServerAddForm.styled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+
+  .submit {
+    width: 15%;
+    align-self: flex-end;
+    margin-top: 0.75rem;
+    height: 2.5rem;
+    font-size: 1.125rem;
+  }
+`;
