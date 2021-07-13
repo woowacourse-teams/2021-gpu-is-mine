a23e4c561d8212cbfc4dc940da9836ae28737116
bigsaigon333 <likepeppermint@gmail.com>
2021-07-13 23:32:04 +0900

    feat: useForm 구현

---
+++ front-end/src/hooks/useForm/useForm.ts
@@ -0,0 +1,68 @@
+import { ChangeEvent, FocusEvent, FormEvent, useEffect, useState } from "react";
+
+type Value = string | number;
+type Values = Record<string, Value>;
+type IsValid = Record<string, boolean>;
+type SubmitAction = (values: Values) => void;
+
+interface InputOptions {
+  name: string;
+  label: string;
+  validator?: (value: Value) => string;
+}
+
+const useForm = (submitAction: SubmitAction) => {
+  const [values, setValues] = useState<Values>({});
+  const [isValid, setIsValid] = useState<IsValid>({});
+
+  const disabled = !Object.values(isValid).every(Boolean);
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    submitAction(values);
+  };
+
+  const useInput = (
+    initialValue: Value,
+    {
+      name,
+      label,
+      validator = (val) => (val === "" ? `${label}을 입력해주세요.` : ""),
+    }: InputOptions
+  ) => {
+    const [validationMessage, setValidationMessage] = useState("");
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) =>
+      setValues({ ...values, [name]: event.target.value });
+
+    const onBlur = (event: FocusEvent<HTMLInputElement>) => {
+      const message = validator?.(event.target.value) ?? "";
+
+      setValidationMessage(message);
+      setIsValid({ ...isValid, [name]: message === "" });
+    };
+
+    useEffect(() => {
+      setValues({ ...values, [name]: initialValue });
+    }, [initialValue, name]);
+
+    return {
+      value: values[name] ?? initialValue,
+      label,
+      onChange,
+      onBlur,
+      validationMessage,
+    };
+  };
+
+  return {
+    values,
+    isValid,
+    useInput,
+    form: { onSubmit },
+    submit: { disabled },
+  };
+};
+
+export default useForm;
