11fb65159382e3d0533ee7716c3be9c56f0b64a5
Wannte <T.wannte@gmail.com>
2021-07-14 20:35:42 +0900

    refactor: MemberResponse에 lab정보 포함하도록 수정

--- back-end/src/main/java/admin/member/dto/MemberResponse.java
+++ back-end/src/main/java/admin/member/dto/MemberResponse.java
@@ -1,19 +1,24 @@
 package admin.member.dto;
 
+import admin.lab.domain.Lab;
+import admin.lab.dto.LabResponse;
 import admin.member.domain.Member;
 
 public class MemberResponse {
     private Long id;
     private String email;
     private String name;
+    private LabResponse labResponse;
 
-    public MemberResponse(Long id, String email, String name) {
+    public MemberResponse(Long id, String email, String name, LabResponse labResponse) {
         this.id = id;
         this.email = email;
         this.name = name;
+        this.labResponse = labResponse;
     }
 
     public static MemberResponse of(Member member){
-        return new MemberResponse(member.getId(), member.getEmail(), member.getName());
+        LabResponse labResponse = LabResponse.of(member.getLab());
+        return new MemberResponse(member.getId(), member.getEmail(), member.getName(), labResponse);
     }
 }
