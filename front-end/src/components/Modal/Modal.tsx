2e06a717e121e60ac85fea4bb7b7924b2d1824ca
2sooy <1401660@naver.com>
2021-07-15 00:20:03 +0900

    refactor: Modal 컴포넌트에 Dimmer 컴포넌트 적용
    - styled 파일 제거

--- front-end/src/components/Modal/Modal.tsx
+++ front-end/src/components/Modal/Modal.tsx
@@ -1,29 +1,21 @@
-import React from "react";
+import { ReactNode, MouseEvent } from "react";
 import Portal from "../Portal/Portal";
-import { ContentsWrapper, Dimmer } from "./Modal.styled";
+import Dimmer from "../Dimmer/Dimmer";
 
 interface ModalProps {
-  children: React.ReactNode;
+  children: ReactNode;
   isOpen: boolean;
   close: () => void;
 }
 
 const Modal = ({ children, isOpen, close }: ModalProps) => {
-  const onClickDimmer = (e: React.MouseEvent) => {
-    if (e.currentTarget === e.target) {
+  const handleDimmerClick = (event: MouseEvent) => {
+    if (event.currentTarget === event.target) {
       close();
     }
   };
 
-  return (
-    <Portal>
-      {isOpen && (
-        <Dimmer onClick={onClickDimmer}>
-          <ContentsWrapper>{children}</ContentsWrapper>
-        </Dimmer>
-      )}
-    </Portal>
-  );
+  return <Portal>{isOpen && <Dimmer onClick={handleDimmerClick}>{children}</Dimmer>}</Portal>;
 };
 
 export default Modal;
