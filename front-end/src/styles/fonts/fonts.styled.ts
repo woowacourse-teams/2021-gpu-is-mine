ceb7a8c7f2d3ac2845ec81f7dc0d90c2d097e16d
bigsaigon333 <likepeppermint@gmail.com>
2021-07-16 03:32:07 +0900

    chore: Noto Sans KR 폰트 self-hosting

--- front-end/src/styles/fonts/fonts.styled.ts
+++ front-end/src/styles/fonts/fonts.styled.ts
@@ -1,58 +1,72 @@
 import { css } from "styled-components";
-import NotoSansKR_Thin from "./noto-sans-kr/NotoSansKR-Thin.woff2";
-import NotoSansKR_Light from "./noto-sans-kr/NotoSansKR-Light.woff2";
-import NotoSansKR_Regular from "./noto-sans-kr/NotoSansKR-Regular.woff2";
-import NotoSansKR_Medium from "./noto-sans-kr/NotoSansKR-Medium.woff2";
-import NotoSansKR_Bold from "./noto-sans-kr/NotoSansKR-Bold.woff2";
-import NotoSansKR_Black from "./noto-sans-kr/NotoSansKR-Black.woff2";
 
-// TODO: 폰트 사이즈 줄이면서 위 아래 여백 제대로 나오게 하기
+import n100w from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-100.woff";
+import n100w2 from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-100.woff2";
+import n300w from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-300.woff";
+import n300w2 from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-300.woff2";
+import n500w from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-500.woff";
+import n500w2 from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-500.woff2";
+import n700w from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-700.woff";
+import n700w2 from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-700.woff2";
+import n900w from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-900.woff";
+import n900w2 from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-900.woff2";
+import n400w from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-regular.woff";
+import n400w2 from "./noto-sans-kr/noto-sans-kr-v13-latin_korean-regular.woff2";
+
+//  format("woff2"):  Chrome 26+, Opera 23+, Firefox 39+
+//  format("woff"): Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+
 const notoSansKR = css`
+  /* noto-sans-kr-100 - latin_korean */
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Thin}) format("woff2");
+    font-style: normal;
     font-weight: 100;
+    src: local("Noto Sans KR"), url(${n100w2}) format("woff2"), url(${n100w}) format("woff");
   }
 
+  /* noto-sans-kr-300 - latin_korean */
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Light}) format("woff2");
+    font-style: normal;
     font-weight: 300;
+    src: local("Noto Sans KR"), url(${n300w2}) format("woff2"), url(${n300w}) format("woff");
   }
 
+  /* noto-sans-kr-regular - latin_korean */
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Regular}) format("woff2");
+    font-style: normal;
     font-weight: 400;
+    src: local("Noto Sans KR"), url(${n400w2}) format("woff2"), url(${n400w}) format("woff");
   }
 
+  /* noto-sans-kr-500 - latin_korean */
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Medium}) format("woff2");
+    font-style: normal;
     font-weight: 500;
+    src: local("Noto Sans KR"), url(${n500w2}) format("woff2"), url(${n500w}) format("woff");
   }
 
+  /* noto-sans-kr-700 - latin_korean */
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Bold}) format("woff2");
+    font-style: normal;
     font-weight: 700;
+    src: local("Noto Sans KR"), url(${n700w2}) format("woff2"), url(${n700w}) format("woff");
   }
 
+  /* noto-sans-kr-900 - latin_korean */
   @font-face {
     font-family: "Noto Sans KR";
-    src: url(${NotoSansKR_Black}) format("woff2");
+    font-style: normal;
     font-weight: 900;
+    src: local("Noto Sans KR"), url(${n900w2}) format("woff2"), url(${n900w}) format("woff");
   }
 `;
 
 const fonts = css`
-  /* ${notoSansKR}  */
-
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@700&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@900&display=swap");
+  ${notoSansKR}
 `;
+
 export default fonts;
