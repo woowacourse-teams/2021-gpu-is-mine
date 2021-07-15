613b91f02c02d028e58e900a47b41c2dc44f9897
bigsaigon333 <likepeppermint@gmail.com>
2021-07-15 14:41:59 +0900

    feat: Navigation 에 Link 추가

--- front-end/src/domains/ManagerNavigation/ManagerNavigation.tsx
+++ front-end/src/domains/ManagerNavigation/ManagerNavigation.tsx
@@ -1,6 +1,8 @@
 import { HTMLAttributes } from "react";
+import { NavLink } from "react-router-dom";
 import Text from "../../components/Text/Text";
 import { StyledManagerNavigation } from "./ManagerNavigation.styled";
+import PATH from "../../constants/path";
 
 type NavigationProps = HTMLAttributes<HTMLElement>;
 
@@ -10,8 +12,12 @@ const ManagerNavigation = (props: NavigationProps) => (
       <Text weight="medium" className="menu__title">
         GPU 서버 관리
       </Text>
-      <Text className="menu__item active">조회</Text>
-      <Text className="menu__item">등록</Text>
+      <NavLink to={PATH.MANAGER.GPU_SERVER.VIEW} activeClassName="active">
+        <Text className="menu__item">조회</Text>
+      </NavLink>
+      <NavLink to={PATH.MANAGER.GPU_SERVER.REGISTER}>
+        <Text className="menu__item">등록</Text>
+      </NavLink>
     </section>
     <section className="menu">
       <Text weight="medium" className="menu__title">
