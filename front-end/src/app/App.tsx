1307bb4555d251048665c670bee8d918dcd331f2
bigsaigon333 <likepeppermint@gmail.com>
2021-07-15 10:39:38 +0900

    feat: react-router-dom 을 이용한 라우팅 도입

--- front-end/src/app/App.tsx
+++ front-end/src/app/App.tsx
@@ -1,11 +1,18 @@
-import GlobalStyle from "../styles/GlobalStyle";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ThemeProvider from "../styles/ThemeProvider";
-import { Wrapper } from "./App.styled";
+import GlobalStyle from "../styles/GlobalStyle";
+import GpuServerView from "../pages/manager/GpuServerView/GpuServerView";
 
 const App = () => (
   <ThemeProvider>
-    <Wrapper>Hello, world! 안녕하세요 반갑습니다</Wrapper>
     <GlobalStyle />
+    <Router>
+      <Switch>
+        <Route path="/">
+          <GpuServerView />
+        </Route>
+      </Switch>
+    </Router>
   </ThemeProvider>
 );
 
