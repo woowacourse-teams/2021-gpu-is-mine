983953e19aafdb3701826d6a413f7dd572d09e82
bigsaigon333 <likepeppermint@gmail.com>
2021-07-15 14:12:29 +0900

    feat: GpuServer 등록폼 페이지 구현

---
+++ front-end/src/pages/manager/GpuServerRegister/GpuServerRegister.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import cx from "classnames";
+import ManagerNavigation from "../../../domains/ManagerNavigation/ManagerNavigation";
+import ManagerHeader from "../../../domains/ManagerHeader/ManagerHeader";
+import ManagerSubHeader from "../../../domains/ManagerSubHeader/ManagerSubHeader";
+import { Container } from "./GpuServerRegister.styled";
+import GpuServerRegisterForm from "../../../domains/GpuServerRegisterForm/GpuServerRegisterForm";
+
+const GpuServerRegister = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const handleClick = () => setIsNavVisible(!isNavVisible);
+
+  const labName = "동동Lab";
+
+  return (
+    <Container>
+      <div className="header">
+        <ManagerHeader labName={labName} />
+      </div>
+      <div className="sub-header">
+        <ManagerSubHeader labName={labName} onClick={handleClick} />
+      </div>
+      <div className={cx("nav", isNavVisible && "nav--visible")}>
+        <ManagerNavigation />
+      </div>
+      <main className="content">
+        <GpuServerRegisterForm />
+      </main>
+      <footer className="footer">
+        <span>All Rights Reserved gpu-is-mine</span>
+      </footer>
+    </Container>
+  );
+};
+
+export default GpuServerRegister;
