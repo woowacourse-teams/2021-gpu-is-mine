b4c8bc5d4c8a6836ccdd813b513b929ba5b85e43
Wannte <T.wannte@gmail.com>
2021-07-15 11:04:36 +0900

    refactor: MemberServiceTest Transactional 추가

--- back-end/src/test/java/admin/member/application/MemberServiceTest.java
+++ back-end/src/test/java/admin/member/application/MemberServiceTest.java
@@ -15,10 +15,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class MemberServiceTest {
 
     @Autowired
