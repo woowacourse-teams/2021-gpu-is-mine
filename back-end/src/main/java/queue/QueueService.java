9dc7852a306448617820063484232c4fc68a0686
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-07-15 13:49:54 +0900

    refactor: 사용하지 않는 메소드 제거

--- back-end/src/main/java/queue/QueueService.java
+++ back-end/src/main/java/queue/QueueService.java
@@ -9,8 +9,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 /**
- * Queue: Default(lab_[labId]) Exchange: Default("jobLearning") bind: QueueName <-> ExchangeName <--
- * routingKey routingKey: Default(routingKey.[labId]
+ * Queue: Default(lab_[labId]), Exchange: Default("jobLearning"), bind: ExchangeName <-> QueueName
+ * routingKey routingKey: Default(routingKey.[labId])
  */
 
 @Service
@@ -18,6 +18,7 @@ public class QueueService {
 
     private static final String LEARNING_EXCHANGE_NAME = "jobLearning";
     private static final String PREFIX_QUEUE_NAME = "lab_";
+    private static final String PREFIX_ROUTING_KEY = "routingKey.";
 
     private RabbitTemplate rabbitTemplate;
     private AmqpAdmin amqpAdmin;
@@ -28,20 +29,6 @@ public class QueueService {
         this.amqpAdmin = amqpAdmin;
     }
 
-    public void createQueue(Long labId) {
-        String queueName = PREFIX_QUEUE_NAME + labId;
-        amqpAdmin.declareQueue(new Queue(queueName, false));
-    }
-
-    public void createExchange(String topicExchangeName) {
-        amqpAdmin.declareExchange(new TopicExchange(topicExchangeName));
-    }
-
-    public void createBind(Queue queue, TopicExchange topicExchange, Long labId) {
-        amqpAdmin.declareBinding(
-            BindingBuilder.bind(queue).to(topicExchange).with("routingKey." + labId));
-    }
-
     public void createLearningConnection(Long labId) {
         createQueue(labId);
         createExchange(LEARNING_EXCHANGE_NAME);
@@ -50,6 +37,7 @@ public class QueueService {
             labId);
     }
 
+    // TODO: JOB 연결시 사용
     public void pushLearningJob(Job job, Long labId) {
         rabbitTemplate.convertAndSend(LEARNING_EXCHANGE_NAME,
             "routingKey." + labId,
@@ -65,5 +53,19 @@ public class QueueService {
     public void deleteQueue(Long labId) {
         String queueName = PREFIX_QUEUE_NAME + labId;
         amqpAdmin.deleteQueue(queueName);
+    }
+
+    private void createQueue(Long labId) {
+        String queueName = PREFIX_QUEUE_NAME + labId;
+        amqpAdmin.declareQueue(new Queue(queueName, false));
+    }
+
+    private void createExchange(String topicExchangeName) {
+        amqpAdmin.declareExchange(new TopicExchange(topicExchangeName));
+    }
+
+    private void createBind(Queue queue, TopicExchange topicExchange, Long labId) {
+        amqpAdmin.declareBinding(
+            BindingBuilder.bind(queue).to(topicExchange).with(PREFIX_ROUTING_KEY + labId));
     }
 }
--- back-end/src/test/java/queue/QueueServiceTest.java
+++ back-end/src/test/java/queue/QueueServiceTest.java
@@ -1,14 +1,12 @@
 package queue;
 
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 public class QueueServiceTest {
 
     @Autowired
