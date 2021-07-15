c091b59b0b284589e56a52cb38da4b40c942868c
2sooy <1401660@naver.com>
2021-07-15 12:18:52 +0900

    fix: useModal 사용시 provider로 감싸지 않았을 경우 처리

--- front-end/src/components/ModalProvider/ModalProvider.tsx
+++ front-end/src/components/ModalProvider/ModalProvider.tsx
@@ -1,10 +1,18 @@
-import {  createContext, useContext } from "react";
+import { createContext, useContext } from "react";
 import useBoolean from "../../hooks/useBoolean/useBoolean";
 import { DialogContextProps, DialogProviderProps } from "../../types/dialog";
 
-const ModalContext = createContext({} as DialogContextProps);
+const ModalContext = createContext<DialogContextProps | null>(null);
 
-export const useModal = () => useContext(ModalContext);
+export const useModal = () => {
+  const context = useContext(ModalContext);
+
+  if (!context) {
+    throw new Error("useContext must be inside a Provider");
+  }
+
+  return context;
+};
 
 const ModalProvider = ({ defaultIsOpen = false, children }: DialogProviderProps) => {
   const [isOpen, open, close] = useBoolean(defaultIsOpen);
