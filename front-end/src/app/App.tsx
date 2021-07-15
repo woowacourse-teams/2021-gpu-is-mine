d1d76a28cc6a7a75c719022bfb204598ba21f7e4
bigsaigon333 <likepeppermint@gmail.com>
2021-07-15 13:26:18 +0900

    feat: 각 페이지에 대한 PATH 상수화

--- front-end/src/app/App.tsx
+++ front-end/src/app/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import ThemeProvider from "../styles/ThemeProvider";
 import GlobalStyle from "../styles/GlobalStyle";
 import GpuServerView from "../pages/manager/GpuServerView/GpuServerView";
+import PATH from "../constants/path";
 
 const App = () => (
   <ThemeProvider>
     <GlobalStyle />
     <Router>
       <Switch>
-        <Route path="/">
+        <Route exact path={[PATH.MANAGER.GPU_SERVER.VIEW, "/"]}>
           <GpuServerView />
         </Route>
+        <Redirect to={PATH.MANAGER.GPU_SERVER.VIEW} />
       </Switch>
     </Router>
   </ThemeProvider>
