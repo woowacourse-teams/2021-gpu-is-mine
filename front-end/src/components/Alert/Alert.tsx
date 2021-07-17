65397bc656d2d4d27fcc648d09b001bda9a1baf3
Lee Soo Yeon <46412689+2SOOY@users.noreply.github.com>
2021-07-17 15:40:03 +0900

    feat: Alert 컴포넌트 구현 (#111)
    * feat: Alert 컴포넌트 구현
    
    - Confirm 컴포넌트와 구조 동일
    
    * feat: Alert 컴포넌트 구현
    
    - Confirm 컴포넌트와 구조 동일
    
    * style: eslint 적용
    
    * style: prettier 적용
    
    Co-authored-by: bigsaigon333 <likepeppermint@gmail.com>

---
+++ front-end/src/components/Alert/Alert.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, MouseEventHandler, MouseEvent } from "react";
+import Button from "../Button/Button";
+import Dimmer from "../Dimmer/Dimmer";
+import Portal from "../Portal/Portal";
+import { AlertWrapper } from "./Alert.styled";
+import { useAlert } from "../AlertProvider/AlertProvider";
+
+interface AlertProps {
+  onConfirm: MouseEventHandler<HTMLButtonElement>;
+  children: ReactNode;
+}
+
+const Alert = ({ children, onConfirm }: AlertProps) => {
+  const { isOpen, close } = useAlert();
+
+  const handleConfirm = (event: MouseEvent<HTMLButtonElement>) => {
+    onConfirm(event);
+    close();
+  };
+
+  return (
+    <Portal>
+      {isOpen && (
+        <Dimmer color="transparent">
+          <AlertWrapper>
+            <div className="content-wrapper">{children}</div>
+            <div className="button-wrapper">
+              <Button className="button" color="secondary-light" onClick={handleConfirm}>
+                확인
+              </Button>
+            </div>
+          </AlertWrapper>
+        </Dimmer>
+      )}
+    </Portal>
+  );
+};
+
+export default Alert;
