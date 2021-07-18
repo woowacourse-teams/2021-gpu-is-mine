9cf5fd90153685ee096934c660deda1e14d11cc0
Wannte <T.wannte@gmail.com>
2021-07-18 17:57:13 +0900

    refactor: GpuBoardException 구조 개편

--- back-end/src/main/java/admin/gpuserver/exception/GpuBoardException.java
+++ back-end/src/main/java/admin/gpuserver/exception/GpuBoardException.java
@@ -1,7 +1,21 @@
 package admin.gpuserver.exception;
 
-public class GpuBoardException extends RuntimeException {
-    public GpuBoardException(String message) {
-        super(message);
+import admin.exception.http.BadRequestException;
+import admin.exception.http.CustomException;
+
+public enum GpuBoardException{
+    INVALID_PERFORMANCE(new BadRequestException("잘못된 GpuBoard 정보 입력입니다.")),
+    INVALID_STATUS(new BadRequestException("GpuBoard 상태는 Null일 수 없습니다.")),
+    INVALID_MODEL(new BadRequestException("적절하지 않은 GpuBoard 모델 정보입니다.")),
+    INVALID_GPU_SERVER_ID(new BadRequestException("GpuBoard의 GpuServer 정보는 Null일 수 없습니다."));
+
+    private CustomException customException;
+
+    GpuBoardException(CustomException e) {
+        this.customException = e;
+    }
+
+    public CustomException getException() {
+        return customException;
     }
 }
