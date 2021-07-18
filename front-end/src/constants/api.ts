ea39a87daf68e519c365ed7fbeb1c59de3d281d8
bigsaigon333 <likepeppermint@gmail.com>
2021-07-18 18:49:54 +0900

    chore: BaseURL https 로 변경

--- front-end/src/constants/api.ts
+++ front-end/src/constants/api.ts
@@ -1,5 +1,4 @@
-const BASE_URL = "http://3.35.169.99:8080/api"; // local
-// const BASE_URL = "https://3.35.169.99/api";// remote
+const BASE_URL = "https://3.35.169.99/api";
 
 const API_ENDPOINT = {
   LABS(labId: number) {
