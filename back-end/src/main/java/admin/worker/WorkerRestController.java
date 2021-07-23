5e53874a562ee7622cebbcbfb918e76a2a929ac2
naeunkim <knae11@naver.com>
2021-07-23 14:15:44 +0900

    style: 포드 포맷팅

--- back-end/src/main/java/admin/worker/WorkerRestController.java
+++ back-end/src/main/java/admin/worker/WorkerRestController.java
@@ -21,14 +21,15 @@ public class WorkerRestController {
 
     // todo: worker가 본인이 실행하는 잡이 없을 때 우리스프링 서버에 get 요청을 함
     @GetMapping("{serverId}/job")
-    public ResponseEntity<JobResponse> takeJob(@PathVariable Long serverId){
+    public ResponseEntity<JobResponse> takeJob(@PathVariable Long serverId) {
         JobResponse jobResponse = jobService.popJobByServerId(serverId);
         return ResponseEntity.ok(jobResponse);
     }
 
     // todo: worker는 n 분마다 우리 서버에게 살아있음을 주기적으로 (put) 알려줌
     @PutMapping("{serverId}/status")
-    public ResponseEntity<Void> updateServerStatus(@PathVariable Long serverId, @RequestBody WorkerStatusRequest workerStatusRequest){
+    public ResponseEntity<Void> updateServerStatus(@PathVariable Long serverId,
+            @RequestBody WorkerStatusRequest workerStatusRequest) {
         jobService.updateWorkerStatus(serverId, workerStatusRequest.getIsOn());
         return ResponseEntity.ok().build();
     }
