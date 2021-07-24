506ca34b57cf516170c2e4942e7f70daca61cbe2
2sooy <1401660@naver.com>
2021-07-24 16:24:57 +0900

    refactor: Job 관련 타입 위치 변경

--- front-end/src/types/gpuServer.ts
+++ front-end/src/types/gpuServer.ts
@@ -1,12 +1,6 @@
-type JobStatus = "WAITING" | "COMPLETED" | "CANCELED" | "RUNNING";
+import { Job } from "./jobs";
 
-interface Job {
-  id: number;
-  name: string;
-  status: JobStatus;
-}
-
-interface GpuBoard {
+export interface GpuBoard {
   id: number;
   performance: number;
   modelName: string;
--- front-end/src/types/jobs.ts
+++ front-end/src/types/jobs.ts
@@ -1,5 +1,13 @@
 import { GpuServer } from "./gpuServer";
 
+type JobStatus = "WAITING" | "COMPLETED" | "CANCELED" | "RUNNING";
+
+export interface Job {
+  id: number;
+  name: string;
+  status: JobStatus;
+}
+
 export interface JobRegisterRequest {
   name: string;
   expectedTime: number;
