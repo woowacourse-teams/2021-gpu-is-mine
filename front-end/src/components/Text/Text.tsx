fd7a929823667ad9a330c5d155a996f876101e12
2sooy <1401660@naver.com>
2021-07-24 13:57:41 +0900

    feat: Text 컴포넌트 다른 tag로 변경할 수 있도록 prop 추가

--- front-end/src/components/Text/Text.tsx
+++ front-end/src/components/Text/Text.tsx
@@ -1,10 +1,14 @@
 import { HTMLAttributes } from "react";
 import { StyledTextProps, StyledText } from "./Text.styled";
 
-type TextProps = HTMLAttributes<HTMLElement> & Partial<StyledTextProps>;
+//TODO tag로 prop을 지정하지 않고 StyledComponent Props를 확장하여 tag prop 제거하기
+type TextProps = HTMLAttributes<HTMLElement> &
+  Partial<StyledTextProps> & {
+    tag?: keyof HTMLElementTagNameMap;
+  };
 
-const Text = ({ size = "md", weight = "regular", children, ...rest }: TextProps) => (
-  <StyledText size={size} weight={weight} {...rest}>
+const Text = ({ size = "md", weight = "regular", tag = "p", children, ...rest }: TextProps) => (
+  <StyledText as={tag} size={size} weight={weight} {...rest}>
     {children}
   </StyledText>
 );
