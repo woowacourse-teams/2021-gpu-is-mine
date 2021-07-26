2af826c20bb948b73f285be2ba9ceb700afe4726
bigsaigon333 <likepeppermint@gmail.com>
2021-07-26 16:17:08 +0900

    feat: Text 컴포넌트에 as prop 추가

--- front-end/src/components/Text/Text.tsx
+++ front-end/src/components/Text/Text.tsx
@@ -1,14 +1,13 @@
 import { HTMLAttributes } from "react";
 import { StyledTextProps, StyledText } from "./Text.styled";
 
-//TODO tag로 prop을 지정하지 않고 StyledComponent Props를 확장하여 tag prop 제거하기
 type TextProps = HTMLAttributes<HTMLElement> &
   Partial<StyledTextProps> & {
-    tag?: keyof HTMLElementTagNameMap;
+    as?: keyof HTMLElementTagNameMap;
   };
 
-const Text = ({ size = "md", weight = "regular", tag = "p", children, ...rest }: TextProps) => (
-  <StyledText as={tag} size={size} weight={weight} {...rest}>
+const Text = ({ size = "md", weight = "regular", as = "p", children, ...rest }: TextProps) => (
+  <StyledText as={as} size={size} weight={weight} {...rest}>
     {children}
   </StyledText>
 );
