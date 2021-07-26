da506ee09254d1a59bb9b120c2bba2b782897c2f
bigsaigon333 <likepeppermint@gmail.com>
2021-07-27 07:54:45 +0900

    refactor: CSS in JS 방식으로 Input style 재작성

--- front-end/src/components/Input/Input.styled.ts
+++ front-end/src/components/Input/Input.styled.ts
@@ -1,75 +1,87 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import Text from "../Text/Text";
 
-export const Label = styled.label`
-  &.sm :not(:first-child) {
-    margin-top: 0.25rem;
-  }
+type Size = "sm" | "md" | "lg";
 
-  &.md :not(:first-child) {
-    margin-top: 0.5rem;
-  }
+export const Label = styled.label<{ size: Size }>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 
-  &.lg :not(:first-child) {
-    margin-top: 0.75rem;
-  }
+  ${({ size }) =>
+    ({
+      sm: css`
+        row-gap: 0.25rem;
+      `,
+      md: css`
+        row-gap: 0.5rem;
+      `,
+      lg: css`
+        row-gap: 0.75rem;
+      `,
+    }[size])}
+`;
 
-  .input {
-    width: 100%;
-    border-style: solid;
-    border-radius: 0.5rem;
-  }
+export const StyledInput = styled.input<{ _size: Size; isValid: boolean }>`
+  width: 100%;
+  border-style: solid;
+  border-radius: 0.5rem;
 
-  .input:focus {
+  &:focus {
     outline: none;
   }
 
-  &.sm .input {
-    padding: 0.25rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5rem;
-    border-width: 2px;
-  }
+  ${({ _size }) =>
+    // eslint-disable-next-line no-underscore-dangle
+    ({
+      sm: css`
+        padding: 0.25rem 0.75rem;
+        font-size: 1rem;
+        line-height: 1.5rem;
+        border-width: 2px;
+      `,
+      md: css`
+        padding: 0.5rem 0.75rem;
+        font-size: 1.25rem;
+        line-height: 1.75rem;
+        border-width: 2px;
+      `,
+      lg: css`
+        padding: 0.5rem 1rem;
+        font-size: 1.875rem;
+        line-height: 2.25rem;
+        border-width: 3px;
+      `,
+    }[_size])}
 
-  &.md .input {
-    padding: 0.5rem 0.75rem;
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    border-width: 2px;
-  }
+  ${({ isValid }) =>
+    isValid
+      ? css`
+          border-color: var(--primary-700);
 
-  &.lg .input {
-    padding: 0.5rem 1rem;
-    font-size: 1.875rem;
-    line-height: 2.25rem;
-    border-width: 3px;
-  }
+          &:focus {
+            border-color: var(--secondary-400);
+          }
+        `
+      : css`
+          border-color: var(--error);
+        `}
+`;
 
-  &.valid .input {
-    border-color: var(--primary-700);
-  }
+export const ValidationMessage = styled(Text)<{ size: "xs" | "sm" | "md" }>`
+  color: var(--error);
+  margin-left: 0.5rem;
 
-  &.valid .input:focus {
-    border-color: var(--secondary-400);
-  }
-
-  :not(&.valid) .input {
-    border-color: var(--error);
-  }
-
-  .validation-message {
-    color: var(--error);
-    margin-left: 0.5rem;
-  }
-
-  &.sm .validation-message {
-    min-height: 1rem;
-  }
-
-  &.md .validation-message {
-    min-height: 1.25rem;
-  }
-
-  &.lg .validation-message {
-    min-height: 1.875rem;
-  }
+  ${({ size }) =>
+    ({
+      xs: css`
+        min-height: 1rem;
+      `,
+      sm: css`
+        min-height: 1.25rem;
+      `,
+      md: css`
+        min-height: 1.875rem;
+      `,
+    }[size])}
 `;
