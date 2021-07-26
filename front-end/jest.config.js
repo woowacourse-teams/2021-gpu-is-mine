3981feef6481a2b073dc7b0236ac44bdec5ca8b9
bigsaigon333 <likepeppermint@gmail.com>
2021-07-27 07:54:04 +0900

    chore: webpack, styled-component 사용시 jest 설정 추가

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
   rootDir: "src",
   moduleDirectories: ["node_modules", "src/__test__"],
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
+  },
 };
