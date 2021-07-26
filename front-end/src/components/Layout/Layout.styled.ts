04a18db96eb40d4a0e2a9b1fe40a06e95e60de2f
bigsaigon333 <likepeppermint@gmail.com>
2021-07-26 15:29:02 +0900

    fix: css 클래스명이 겹치지 않게 styled component화

--- front-end/src/components/Layout/Layout.styled.ts
+++ front-end/src/components/Layout/Layout.styled.ts
@@ -10,55 +10,55 @@ export const StyledLayout = styled.div`
   ${up("tablet")} {
     grid-template: auto 1fr auto / auto 1fr;
   }
+`;
 
-  .header {
+export const HeaderContainer = styled.div`
+  grid-row: 1 / 2;
+`;
+
+export const SubHeaderContainer = styled.div`
+  grid-row: 2 / 3;
+
+  ${up("tablet")} {
     grid-row: 1 / 2;
+    grid-column: 2 / 4;
   }
+`;
 
-  .sub-header {
+export const NavContainer = styled.div`
+  display: none;
+
+  ${up("tablet")} {
+    display: inherit;
+
+    grid-column: 1 / 2;
+    grid-row: 2 / 4;
+  }
+`;
+
+export const ContentContainer = styled.div`
+  grid-row: 4 / 5;
+  overflow-y: auto;
+  padding: 1rem;
+  background-color: var(--primary-50);
+
+  ${up("tablet")} {
+    grid-column: 2 / 3;
     grid-row: 2 / 3;
-
-    ${up("tablet")} {
-      grid-row: 1 / 2;
-      grid-column: 2 / 4;
-    }
-  }
-
-  .nav {
-    display: none;
-
-    ${up("tablet")} {
-      display: inherit;
-
-      grid-column: 1 / 2;
-      grid-row: 2 / 4;
-    }
-  }
-
-  .content {
-    grid-row: 4 / 5;
-    overflow-y: auto;
-    padding: 1rem;
-    background-color: var(--primary-50);
-
-    ${up("tablet")} {
-      grid-column: 2 / 3;
-      grid-row: 2 / 3;
-      display: flex;
-    }
-  }
-
-  .footer {
-    grid-row: 5 / 6;
-    background-color: var(--primary-50);
-    width: 100%;
     display: flex;
-    justify-content: center;
-    align-items: center;
+  }
+`;
 
-    ${up("tablet")} {
-      grid-column: 2 / 3;
-      grid-row: 3 / 4;
-    }
+export const FooterContainer = styled.div`
+  grid-row: 5 / 6;
+  background-color: var(--primary-50);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${up("tablet")} {
+    grid-column: 2 / 3;
+    grid-row: 3 / 4;
   }
 `;
