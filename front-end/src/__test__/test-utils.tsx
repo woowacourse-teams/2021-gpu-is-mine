11f3f9f706aa39101665e64dcad2895769cfecdf
bigsaigon333 <likepeppermint@gmail.com>
2021-07-27 14:46:32 +0900

    chore: test-utils 에 userEvent default export 추가

--- front-end/src/__test__/test-utils.tsx
+++ front-end/src/__test__/test-utils.tsx
@@ -9,4 +9,6 @@ export * from "@testing-library/react";
 
 export * from "@testing-library/user-event";
 
+export { default as userEvent } from "@testing-library/user-event";
+
 export { customRender as render };
