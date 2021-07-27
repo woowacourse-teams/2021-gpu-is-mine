4ae0cb3069f718faee02356eee055f3079107365
bigsaigon333 <likepeppermint@gmail.com>
2021-07-27 10:34:03 +0900

    refactor: React.FC 타입 제거

--- front-end/src/providers/Providers.tsx
+++ front-end/src/providers/Providers.tsx
@@ -1,8 +1,12 @@
-import { FC } from "react";
+import { ReactNode } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle, ThemeProvider } from "../styles";
 
-const Providers: FC = ({ children }) => (
+interface ProvidersProps {
+  children?: ReactNode;
+}
+
+const Providers = ({ children }: ProvidersProps): JSX.Element => (
   <ThemeProvider>
     <GlobalStyle />
     <Router>{children}</Router>
