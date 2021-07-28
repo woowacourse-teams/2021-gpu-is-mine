c77c580a7ad004f8d258f8b8fdb88338fc1718b4
2sooy <1401660@naver.com>
2021-07-28 23:57:03 +0900

    refactor: Job관련 타입 import & rexport

--- front-end/src/__fixtures__/index.ts
+++ front-end/src/__fixtures__/index.ts
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as gpuServersResponses } from "./gpuServersResponses";
+export { default as jobsResponsesMock } from "./jobsResponses";
