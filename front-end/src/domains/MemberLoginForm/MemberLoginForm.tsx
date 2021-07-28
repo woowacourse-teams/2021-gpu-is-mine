034910132f83b7731b06f02a013a5c05b3229bae
2sooy <1401660@naver.com>
2021-07-28 17:04:56 +0900

    feat: initial commit

---
+++ front-end/src/domains/MemberLoginForm/MemberLoginForm.tsx
@@ -0,0 +1,9 @@
+import { StyledForm } from "./MemberLoginForm.styled";
+
+interface MemberLoginFormProps {}
+
+const MemberLoginForm = ({}: MemberLoginFormProps) => {
+  return <StyledForm></StyledForm>;
+};
+
+export default MemberLoginForm;
