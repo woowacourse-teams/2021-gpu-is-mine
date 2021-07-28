a27072f45a0feb155b0679399144d5311536e808
bigsaigon333 <likepeppermint@gmail.com>
2021-07-28 20:37:32 +0900

    refactor: emailValidator 를  isEmail로 추상화

--- front-end/src/utils/validator.ts
+++ front-end/src/utils/validator.ts
@@ -12,3 +12,10 @@ export const isLength = (
   value: string,
   { min = 0, max = Number.MAX_SAFE_INTEGER }: ValidatorOption
 ) => min <= value.length && value.length <= max;
+
+export const isEmail = (value: string) => {
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return emailRegex.test(value);
+};
