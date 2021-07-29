3250851264044a523eba8c2a975266ce4912442c
wannte <49307266+wannte@users.noreply.github.com>
2021-07-29 16:33:56 +0900

    feature: email기능 구현 (#184)
    * chore: submodule 등록 및 build copy task 추가
    
    * feat: 기본 Mail 등록 기능 구현
    
    * chore: task depends on 추가
    
    * chore: submodule 등록 및 build copy task 추가
    
    * feat: 기본 Mail 등록 기능 구현
    
    * chore: task depends on 추가
    
    * feat: async mail sending
    
    * feat: template 메일 서비스에 연결
    
    * refactor: github action submoduel update
    
    * refactor: 피드백 반영
    
    * chore: private에 from 이메일 정보 추가
    
    * feat: Mail 서비스 Controller에 연결
    
    * feat: 이메일 전송 기능 추가
    
    * feat: Mail Service WorkerService 연동
    
    * chore: 괄호 닫기
    
    Co-authored-by: MyaGya <38939015+MyaGya@users.noreply.github.com>

--- back-end/src/main/java/admin/job/ui/JobController.java
+++ back-end/src/main/java/admin/job/ui/JobController.java
@@ -1,11 +1,12 @@
 package admin.job.ui;
 
 import admin.auth.domain.AuthenticationPrincipal;
-import admin.gpuserver.application.GpuServerService;
 import admin.job.application.JobService;
 import admin.job.dto.request.JobRequest;
 import admin.job.dto.response.JobResponse;
 import admin.job.dto.response.JobResponses;
+import admin.mail.MailDto;
+import admin.mail.MailService;
 import admin.member.application.MemberService;
 import admin.member.domain.Member;
 import java.net.URI;
@@ -22,20 +23,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/labs/{labId}")
 public class JobController {
-
     private MemberService memberService;
     private JobService jobService;
+    private MailService mailService;
 
-    public JobController(MemberService memberService, JobService jobService) {
+    public JobController(MemberService memberService, JobService jobService, MailService mailService) {
         this.memberService = memberService;
         this.jobService = jobService;
+        this.mailService = mailService;
     }
 
     @PostMapping("/jobs")
     public ResponseEntity<Void> save(@PathVariable Long labId, @AuthenticationPrincipal Member member,
             @RequestBody JobRequest jobRequest) {
         Long jobId = jobService.save(member.getId(), jobRequest);
-
+        mailService.sendJobReserveMail(new MailDto(member.getEmail(), jobRequest.getName()));
         URI uri = URI.create("/api/labs/" + labId + "/jobs/" + jobId);
         return ResponseEntity.created(uri).build();
     }
@@ -64,8 +66,9 @@ public class JobController {
     @PutMapping("/jobs/{jobId}")
     public ResponseEntity<Void> cancel(@PathVariable Long jobId, @AuthenticationPrincipal Member member) {
         memberService.checkEditableJob(member.getId(), jobId);
-
+        JobResponse job = jobService.findById(jobId);
         jobService.cancel(jobId);
+        mailService.sendJobCancelMail(new MailDto(member.getEmail(), job.getName()));
         return ResponseEntity.noContent().build();
     }
 }
--- back-end/src/main/java/admin/worker/ui/WorkerController.java
+++ back-end/src/main/java/admin/worker/ui/WorkerController.java
@@ -1,6 +1,10 @@
 package admin.worker.ui;
 
+import admin.job.application.JobService;
+import admin.job.domain.JobStatus;
 import admin.job.dto.response.JobResponse;
+import admin.mail.MailDto;
+import admin.mail.MailService;
 import admin.worker.application.WorkerService;
 import admin.worker.dto.WorkerJobLogRequest;
 import admin.worker.dto.WorkerJobRequest;
@@ -18,11 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/workers/")
 public class WorkerController {
-
     private final WorkerService workerService;
+    private final MailService mailService;
+    private final JobService jobService;
 
-    public WorkerController(WorkerService workerService) {
+    public WorkerController(WorkerService workerService, MailService mailService,
+            JobService jobService) {
         this.workerService = workerService;
+        this.mailService = mailService;
+        this.jobService = jobService;
     }
 
     @GetMapping("gpus/{serverId}/job")
@@ -35,6 +43,29 @@ public class WorkerController {
     public ResponseEntity<Void> updateJobStatus(@PathVariable Long jobId,
             @RequestBody WorkerJobRequest workerJobRequest) {
         workerService.updateJobStatus(jobId, workerJobRequest);
+        MailDto mailDto = jobService.mailDtoOfJob(jobId);
+        if (workerJobRequest.getJobStatus() == JobStatus.RUNNING) {
+            mailService.sendJobStartMail(mailDto);
+        }
+        if (workerJobRequest.getJobStatus() == JobStatus.WAITING) {
+            mailService.sendJobEndMail(mailDto);
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("jobs/{jobId}/start")
+    public ResponseEntity<Void> start(@PathVariable Long jobId) {
+        workerService.start(jobId);
+        MailDto mailDto = jobService.mailDtoOfJob(jobId);
+        mailService.sendJobStartMail(mailDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("jobs/{jobId}/end")
+    public ResponseEntity<Void> end(@PathVariable Long jobId) {
+        workerService.end(jobId);
+        MailDto mailDto = jobService.mailDtoOfJob(jobId);
+        mailService.sendJobStartMail(mailDto);
         return ResponseEntity.ok().build();
     }
 
@@ -47,9 +78,8 @@ public class WorkerController {
 
     @PostMapping("jobs/{jobId}/log")
     public ResponseEntity<Void> saveLog(@PathVariable Long jobId,
-        @RequestBody WorkerJobLogRequest workerJobLogRequest) {
+            @RequestBody WorkerJobLogRequest workerJobLogRequest) {
         Long logId = workerService.saveLog(jobId, workerJobLogRequest);
-
         URI uri = URI.create("/api/workers/jobs/" + jobId + "/log/" + logId);
         return ResponseEntity.created(uri).build();
     }
