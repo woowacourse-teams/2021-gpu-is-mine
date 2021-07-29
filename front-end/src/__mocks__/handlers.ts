1ee0242d887b43d4377f995aa6435b91758ea9c0
bigsaigon333 <likepeppermint@gmail.com>
2021-07-29 12:42:45 +0900

    test: 로그인 mock server handler 구현

--- front-end/src/__mocks__/handlers.ts
+++ front-end/src/__mocks__/handlers.ts
@@ -1,6 +1,11 @@
 import { rest } from "msw";
 import { API_ENDPOINT } from "../constants";
 
-const handlers = [rest.post(API_ENDPOINT.MEMBERS, (_, res, ctx) => res(ctx.status(201)))];
+const handlers = [
+  rest.post(API_ENDPOINT.MEMBERS, (_, res, ctx) => res(ctx.status(201))),
+  rest.post(API_ENDPOINT.LOGIN, (_, res, ctx) =>
+    res(ctx.json({ accessToken: "access-token" }), ctx.status(200))
+  ),
+];
 
 export default handlers;
