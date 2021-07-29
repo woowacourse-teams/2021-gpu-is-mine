1137901e83735ae9546118d5445ef1a0744f8a7c
bigsaigon333 <likepeppermint@gmail.com>
2021-07-29 23:01:38 +0900

    feat: useAuth 구현

--- front-end/src/hooks/useAuth/useAuth.ts
+++ front-end/src/hooks/useAuth/useAuth.ts
@@ -1,13 +1,15 @@
-import { createContext, useContext } from "react";
+import { createContext, useCallback, useContext, useEffect } from "react";
 import useBoolean from "../useBoolean/useBoolean";
 import useFetch from "../useFetch/useFetch";
 import { API_ENDPOINT } from "../../constants";
-import { MemberLoginResponse, MemberLoginRequest } from "../../types";
+import { MemberLoginResponse, MemberLoginRequest, MyInfoResponse } from "../../types";
 
 interface AuthContext {
   isAuthenticated: boolean;
-  login: ({ email, password }: { email: string; password: string }) => Promise<unknown | Error>;
-  logout: (arg: never) => Promise<unknown | Error>;
+  login: ({ email, password }: { email: string; password: string }) => Promise<unknown>;
+  logout: (arg: never) => Promise<unknown>;
+  isLoading: boolean;
+  myInfo: MyInfoResponse | null;
 }
 
 export const authContext = createContext<AuthContext | null>(null);
@@ -22,31 +24,50 @@ export const useAuth = () => {
   return cxt;
 };
 
+const useGetMyInfo = () =>
+  useFetch<MyInfoResponse>(API_ENDPOINT.ME, {
+    method: "get",
+  });
+
+const usePostLogin = () =>
+  useFetch<MemberLoginResponse, MemberLoginRequest>(API_ENDPOINT.LOGIN, {
+    method: "post",
+  });
+
+export const useRequest = () => {
+  const { makeRequest: requestLogin, status: loginStatus } = usePostLogin();
+
+  const { makeRequest: fetchMyInfo, status: myInfoStatus, data: myInfo } = useGetMyInfo();
+
+  const isLoading = [loginStatus, myInfoStatus].some((status) => status === "loading");
+
+  return { requestLogin, fetchMyInfo, myInfo, isLoading };
+};
+
 export const useAuthProvider = () => {
   const [isAuthenticated, authenticate, unauthenticate] = useBoolean(false);
 
-  const { makeRequest: loginRequest } = useFetch<MemberLoginResponse, MemberLoginRequest>(
-    API_ENDPOINT.LOGIN,
-    {
-      method: "post",
-    }
-  );
+  const { requestLogin, fetchMyInfo, myInfo, isLoading } = useRequest();
 
   const login = async ({ email, password }: { email: string; password: string }) => {
-    const { accessToken } = await (await loginRequest({ email, password })).unwrap();
-
-    authenticate();
+    const { accessToken } = await (await requestLogin({ email, password })).unwrap();
 
     sessionStorage.setItem("accessToken", accessToken);
+
+    authenticate();
   };
 
-  const logout = async () => {
-    // useFetch logout
-
+  const logout = useCallback(async () => {
     unauthenticate();
 
     sessionStorage.removeItem("accessToken");
-  };
+  }, [unauthenticate]);
 
-  return { isAuthenticated, login, logout };
+  useEffect(() => {
+    if (sessionStorage.getItem("accessToken")) {
+      fetchMyInfo().then(authenticate).catch(logout);
+    }
+  }, [authenticate, fetchMyInfo, logout]);
+
+  return { isAuthenticated, login, logout, isLoading, myInfo };
 };
