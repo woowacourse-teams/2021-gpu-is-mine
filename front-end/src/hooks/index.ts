d5f077b834d0c2b86d5cd03840fa8927c0e5198d
bigsaigon333 <likepeppermint@gmail.com>
2021-08-01 23:35:36 +0900

    refactor: 주어진 route로 이동하는 useMoveToPage hook 추가

--- front-end/src/hooks/index.ts
+++ front-end/src/hooks/index.ts
@@ -1,8 +1,9 @@
 export * from "./useApi/useApi";
 export { useAuth } from "./useAuth/useAuth";
-export { default as useLabId } from "./useLabId/useLabId";
 export { default as useBoolean } from "./useBoolean/useBoolean";
 export { default as useBreakpoints } from "./useBreakpoints/useBreakpoints";
 export { default as useFetch } from "./useFetch/useFetch";
 export { default as useForm, Values, SubmitAction } from "./useForm/useForm";
+export * from "./useHistory/useHistory";
+export { default as useLabId } from "./useLabId/useLabId";
 export { default as useToggle } from "./useToggle/useToggle";
