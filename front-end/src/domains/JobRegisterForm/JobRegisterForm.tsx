5e46d87ce02062134af8db1a9771ea12818ddc9d
bigsaigon333 <likepeppermint@gmail.com>
2021-08-02 00:30:31 +0900

    refactor: JobRegsiterFrom validator와 컴포넌트 분리

--- front-end/src/domains/JobRegisterForm/JobRegisterForm.tsx
+++ front-end/src/domains/JobRegisterForm/JobRegisterForm.tsx
@@ -1,46 +1,13 @@
 import { FormHTMLAttributes } from "react";
+import { jobNameValidator, expectedTimeValidator, minPerformanceValidator } from "./validator";
 import { useFetch, useForm, Values } from "../../hooks";
 import { Alert, Button, Dimmer, Input, Loading, Text } from "../../components";
 import JobRegisterRadioGroup from "../JobRegisterRadioGroup/JobRegisterRadioGroup";
 import { StyledForm } from "./JobRegisterForm.styled";
-import { APICallStatus, JobRegisterRequest } from "../../types";
-import { isLength, isNumber } from "../../utils";
 import { API_ENDPOINT } from "../../constants";
+import { APICallStatus, JobRegisterRequest } from "../../types";
 
 type JobRegisterFormProps = FormHTMLAttributes<HTMLFormElement>;
-
-const jobNameValidator = (value: string) => {
-  const min = 1;
-  const max = 30;
-
-  if (!isLength(value, { min, max })) {
-    return `${min}글자 이상 ${max}글자 이하만 가능합니다.`;
-  }
-
-  return null;
-};
-
-const expectedTimeValidator = (value: string) => {
-  const min = 0;
-  const max = 10_000;
-
-  if (!isNumber(value, { min, max })) {
-    return `${min} 시간 이상 ${max} 시간 이하만 가능합니다.`;
-  }
-
-  return null;
-};
-
-const minPerformanceValidator = (value: string) => {
-  const min = 0;
-  const max = 100_000;
-
-  if (!isNumber(value, { min, max })) {
-    return `${min} TFLOPS 이상 ${max} TFLOPS 이하만 가능합니다.`;
-  }
-
-  return null;
-};
 
 const JobRegisterForm = (props: JobRegisterFormProps) => {
   const { status, makeRequest, done } = useFetch<void, JobRegisterRequest>(
