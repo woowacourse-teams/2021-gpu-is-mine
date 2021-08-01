02619ae5fe9430a59c4d688d3d467945991df356
bigsaigon333 <likepeppermint@gmail.com>
2021-08-01 17:21:02 +0900

    test: jobDetail jobId params mocking

--- front-end/src/domains/JobDetail/JobDetail.stories.tsx
+++ front-end/src/domains/JobDetail/JobDetail.stories.tsx
@@ -1,9 +1,17 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { MemoryRouter, Route } from "react-router-dom";
 import JobDetail from "./JobDetail";
 
 export default {
   title: "Domains/Job/Detail",
   component: JobDetail,
+  decorators: [
+    (story) => (
+      <MemoryRouter initialEntries={["/8"]}>
+        <Route path="/:jobId">{story()}</Route>
+      </MemoryRouter>
+    ),
+  ],
 } as ComponentMeta<typeof JobDetail>;
 
 const Template: ComponentStory<typeof JobDetail> = (args) => <JobDetail {...args} />;
