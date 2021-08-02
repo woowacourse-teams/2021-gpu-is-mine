b21010e2f81106d4e5314197d3d6b4afc8005e9b
bigsaigon333 <likepeppermint@gmail.com>
2021-08-02 11:30:36 +0900

    refactor: Job 등록폼 useFormNew 이용하여 Refactoring

--- front-end/src/domains/JobRegisterForm/JobRegisterForm.tsx
+++ front-end/src/domains/JobRegisterForm/JobRegisterForm.tsx
@@ -1,6 +1,6 @@
 import { FormHTMLAttributes } from "react";
-import { jobNameValidator, expectedTimeValidator, minPerformanceValidator } from "./validator";
-import { useForm, Values, usePostJobRegister } from "../../hooks";
+import { usePostJobRegister } from "../../hooks";
+import useJobRegisterForm from "./useJobRegisterForm";
 import { Alert, Button, Dimmer, Input, Loading, Text } from "../../components";
 import { StyledForm } from "./JobRegisterForm.styled";
 import JobRegisterRadioGroup from "../JobRegisterRadioGroup/JobRegisterRadioGroup";
@@ -12,42 +12,15 @@ interface JobRegisterFormProps extends FormHTMLAttributes<HTMLFormElement> {
 const JobRegisterForm = ({ labId, ...rest }: JobRegisterFormProps) => {
   const { status, makeRequest, done } = usePostJobRegister({ labId });
 
-  const submitAction = async ({ jobName, expectedTime, gpuServerId }: Values) => {
-    const requestBody = {
-      name: String(jobName),
-      expectedTime: Number(expectedTime),
-      gpuServerId: Number(gpuServerId),
-      metaData: "helloworld",
-    };
-
-    return (await makeRequest(requestBody)).unwrap();
-  };
-
-  const { form, reset, useInput } = useForm(submitAction);
-
-  const jobNameInputProps = useInput("", {
-    name: "jobName",
-    label: "Job 이름",
-    validator: jobNameValidator,
-  });
-  const expectedTimeInputProps = useInput("", {
-    name: "expectedTime",
-    label: "Job 예상 실행 시간(hour)",
-    validator: expectedTimeValidator,
-  });
-  const minPerformanceInputProps = useInput("", {
-    name: "minPerformance",
-    label: "최소 필요 연산량(TFLOPS)",
-    validator: minPerformanceValidator,
-  });
-  const metaDataInputProps = useInput("", {
-    name: "metaData",
-    label: "Docker Hub Url",
-  });
-  const gpuServerSelectProps = useInput("", {
-    name: "gpuServerId",
-    label: "GPU 서버 선택",
-  });
+  const {
+    form,
+    reset,
+    jobNameInputProps,
+    expectedTimeInputProps,
+    minPerformanceInputProps,
+    metaDataInputProps,
+    gpuServerSelectProps,
+  } = useJobRegisterForm(makeRequest);
 
   const handleConfirm = () => {
     reset();
