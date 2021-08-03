9e10696d62b9189607adb4ba509f9b1a1056fedd
bigsaigon333 <likepeppermint@gmail.com>
2021-08-03 16:13:14 +0900

    feat: key가 주어진 경우에만  caching

--- front-end/src/utils/axios.ts
+++ front-end/src/utils/axios.ts
@@ -1,18 +1,16 @@
-import axios, { AxiosResponse } from "axios";
-
-// const cache = new Map<string, { lastFetchedTime: number; data: unknown }>();
+import axios, { AxiosRequestConfig } from "axios";
+import { SESSION_STORAGE_KEY } from "../constants";
 
 type RequestConfig<U> = {
   method: "get" | "post" | "head" | "delete" | "options" | "post" | "put" | "patch";
   body?: U;
+  key?: string;
 };
-
-// const REFRESH_PERIOD_IN_MS = 60_000;
 
 const httpClient = axios.create();
 
 httpClient.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem("accessToken");
+  const token = sessionStorage.getItem(SESSION_STORAGE_KEY.ACCESS_TOKEN);
 
   if (token) {
     // eslint-disable-next-line
@@ -22,28 +20,42 @@ httpClient.interceptors.request.use((config) => {
   return config;
 });
 
+const REFRESH_PERIOD_IN_MS = 60_000;
+
+const cache = new Map<string, { lastFetchedTime: number; data: unknown }>();
+
+const fetch = async <T>(url: string, { method, data }: AxiosRequestConfig) =>
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  (await httpClient(url, { method, data })).data as T;
+
 // eslint-disable-next-line import/prefer-default-export
 export const getData = async <T = void, U = never>(
   url: string,
   config?: RequestConfig<U>
 ): Promise<T> => {
-  const { method = "get", body } = config ?? {};
+  const { method = "get", body, key } = config ?? {};
 
-  // const cached = cache.get(url);
+  if (!key) {
+    return fetch(url, { method, data: body });
+  }
 
-  // if (method === "get" && cached && Date.now() - cached.lastFetchedTime <= REFRESH_PERIOD_IN_MS) {
-  //   return cached.data as T;
-  // }
+  if (method === "get") {
+    const cached = cache.get(key);
 
-  // const response = (await httpClient(url, { method, data: body })) as AxiosResponse<T>;
+    if (cached && Date.now() - cached.lastFetchedTime <= REFRESH_PERIOD_IN_MS) {
+      return cached.data as T;
+    }
 
-  // if (method === "get") {
-  //   cache.set(url, { lastFetchedTime: Date.now(), data: response.data });
-  // } else {
-  //   cache.delete(url);
-  // }
+    const data = await fetch<T>(url, { method, data: body });
 
-  const response = (await httpClient(url, { method, data: body })) as AxiosResponse<T>;
+    cache.set(key, { lastFetchedTime: Date.now(), data });
 
-  return response.data;
+    return data;
+  }
+
+  const data = await fetch<T>(url, { method, data: body });
+
+  cache.delete(key);
+
+  return data;
 };
