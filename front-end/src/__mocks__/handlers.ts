b2e0cf45f909edba8c4adab18f2fdd5b7111ef7b
bigsaigon333 <likepeppermint@gmail.com>
2021-08-04 21:43:31 +0900

    test: 로그인 fetchMyInfo 위치수정에 따른 테스트 로직 변경

--- front-end/src/__mocks__/handlers.ts
+++ front-end/src/__mocks__/handlers.ts
@@ -1,14 +1,14 @@
 import { rest } from "msw";
+import { membersMeResponse } from "../__fixtures__";
 import { API_ENDPOINT } from "../constants";
-import { JobResponseMock } from "../__fixtures__/jobsResponses";
 
 const handlers = [
   rest.post(API_ENDPOINT.MEMBER.SIGNUP, (_, res, ctx) => res(ctx.status(201))),
   rest.post(API_ENDPOINT.MEMBER.LOGIN, (_, res, ctx) =>
     res(ctx.json({ accessToken: "access-token" }), ctx.status(200))
   ),
-  rest.get(API_ENDPOINT.MEMBER.LOGIN, (_, res, ctx) =>
-    res(ctx.json(JobResponseMock), ctx.status(200))
+  rest.get(API_ENDPOINT.MEMBER.ME, (_, res, ctx) =>
+    res(ctx.json(membersMeResponse), ctx.status(200))
   ),
 ];
 
