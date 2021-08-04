eef6284005a3ed32cb3aed85929ae2ffceaa9768
sjpark-dev <sjpark.dev@gmail.com>
2021-08-04 17:02:35 +0900

    refactor: GpuServer update 정책 변경

--- back-end/src/test/java/mine/is/gpu/gpuserver/fixture/GpuServerFixtures.java
+++ back-end/src/test/java/mine/is/gpu/gpuserver/fixture/GpuServerFixtures.java
@@ -2,21 +2,30 @@ package mine.is.gpu.gpuserver.fixture;
 
 import mine.is.gpu.gpuserver.dto.request.GpuBoardRequest;
 import mine.is.gpu.gpuserver.dto.request.GpuServerRequest;
-import mine.is.gpu.gpuserver.dto.request.GpuServerUpdateRequest;
 
 public class GpuServerFixtures {
 
     private static final String GPU_BOARD_NAME = "BOARD_NAME";
+    private static final String NEW_GPU_BOARD_NAME = "NEW_BOARD_NAME";
+
     private static final Long GPU_BOARD_PERFORMANCE = 10L;
+    private static final Long NEW_GPU_BOARD_PERFORMANCE = 20L;
 
     private static final String GPU_SERVER_NAME = "SERVER_NAME";
     private static final String NEW_GPU__SERVER_NAME = "NEW_SERVER_NAME";
 
     private static final Long GPU_SERVER_MEMORY_SIZE = 10L;
+    private static final Long NEW_SERVER_MEMORY_SIZE = 20L;
+
     private static final Long GPU_SERVER_DISK_SIZE = 10L;
+    private static final Long NEW_GPU_SERVER_DISK_SIZE = 20L;
 
     public static GpuBoardRequest gpuBoardCreationRequest() {
         return new GpuBoardRequest(GPU_BOARD_NAME, GPU_BOARD_PERFORMANCE);
+    }
+
+    public static GpuBoardRequest gpuBoardUpdateRequest() {
+        return new GpuBoardRequest(NEW_GPU_BOARD_NAME, NEW_GPU_BOARD_PERFORMANCE);
     }
 
     public static GpuServerRequest gpuServerCreationRequest() {
@@ -28,7 +37,12 @@ public class GpuServerFixtures {
         );
     }
 
-    public static GpuServerUpdateRequest gpuServerUpdateRequest() {
-        return new GpuServerUpdateRequest(NEW_GPU__SERVER_NAME);
+    public static GpuServerRequest gpuServerUpdateRequest() {
+        return new GpuServerRequest(
+                NEW_GPU__SERVER_NAME,
+                NEW_SERVER_MEMORY_SIZE,
+                NEW_GPU_SERVER_DISK_SIZE,
+                gpuBoardUpdateRequest()
+        );
     }
 }
