3fb0fcd87559d765004e944b4e271ff41984259f
2sooy <1401660@naver.com>
2021-08-05 12:09:48 +0900

    feat: path 변경 시 url 검증 로직 추가

--- front-end/src/hooks/useHistory/useHistory.ts
+++ front-end/src/hooks/useHistory/useHistory.ts
@@ -1,10 +1,24 @@
-import { useHistory } from "react-router-dom";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { To } from "history";
+import { useHistory, matchPath } from "react-router-dom";
+import { PATH } from "../../constants";
+
+const pathList = [
+  Object.values(PATH.MANAGER.GPU_SERVER),
+  Object.values(PATH.MANAGER.JOB),
+  Object.values(PATH.MEMBER),
+].flat();
 
 // eslint-disable-next-line import/prefer-default-export
-export const useMoveToPage = (route: To) => {
+export const useMoveToPage = (route: string) => {
   const history = useHistory();
+
+  const match = matchPath(route, {
+    path: pathList,
+    exact: true,
+  });
+
+  if (!match) {
+    throw new Error("정의되지 않은 url 입니다.");
+  }
 
   return () => history.push(route);
 };
