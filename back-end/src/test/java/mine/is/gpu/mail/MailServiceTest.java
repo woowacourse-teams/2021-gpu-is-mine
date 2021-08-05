0de3bef4df404d2c81cd4e5fd40c08113bdd3acf
sjpark-dev <sjpark.dev@gmail.com>
2021-08-05 23:06:48 +0900

    refactor: 중복 체크 관련 테스트 코드 추가 수정

--- back-end/src/test/java/mine/is/gpu/mail/MailServiceTest.java
+++ back-end/src/test/java/mine/is/gpu/mail/MailServiceTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class MailServiceTest {
 
     @Autowired
