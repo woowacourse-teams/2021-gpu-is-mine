ce822e6c640df296e22c1a7197b3ae2004dc0bab
bigsaigon333 <likepeppermint@gmail.com>
2021-08-07 00:10:24 +0900

    feat: GpuServer 상세정보 모두 표시

--- front-end/src/domains/GpuServerDetail/GpuServerDetail.test.tsx
+++ front-end/src/domains/GpuServerDetail/GpuServerDetail.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
-
 import GpuServerDetail from "./GpuServerDetail";
-import { gpuServerResponse } from "../../__fixtures__/gpuServersResponses";
+import { gpuServerResponse } from "../../__fixtures__";
 
 jest.mock("../../hooks", () => ({ useGetGpuServerById: () => ({ data: gpuServerResponse }) }));
 
@@ -9,9 +8,28 @@ describe("GpuServerDetail", () => {
   test("data", () => {
     render(<GpuServerDetail labId={1} />);
 
-    expect(screen.getByText(/GPU 연산량/)).toBeInTheDocument();
+    expect(screen.getByText(/서버 이름/)).toBeInTheDocument();
+    expect(screen.getByText(gpuServerResponse.serverName)).toBeInTheDocument();
+
+    expect(screen.getByText(/서버 상태/)).toBeInTheDocument();
+    expect(screen.getByText(gpuServerResponse.isOn ? /ON/ : /OFF/)).toBeInTheDocument();
+
+    expect(screen.getByText(/RAM 용량/)).toBeInTheDocument();
+    expect(screen.getByText(gpuServerResponse.memorySize)).toBeInTheDocument();
+
+    expect(screen.getByText(/디스크 용량/)).toBeInTheDocument();
+    expect(screen.getByText(gpuServerResponse.diskSize)).toBeInTheDocument();
+
+    expect(screen.getByText(/GPU 장치명/)).toBeInTheDocument();
+    expect(screen.getByText(gpuServerResponse.gpuBoard.modelName)).toBeInTheDocument();
+
+    expect(screen.getByText(/성능/)).toBeInTheDocument();
     expect(screen.getByText(gpuServerResponse.gpuBoard.performance)).toBeInTheDocument();
+
     expect(screen.getByText(/현재 실행중인 Job/)).toBeInTheDocument();
+    expect(screen.getByText(/가짜 뉴스 검증을 위한 댓글 분류 학습/)).toBeInTheDocument();
+
     expect(screen.getByText(/대기 중인 Job/)).toBeInTheDocument();
+    expect(screen.getByText(/1개/)).toBeInTheDocument();
   });
 });
--- front-end/src/domains/GpuServerDetail/GpuServerDetail.tsx
+++ front-end/src/domains/GpuServerDetail/GpuServerDetail.tsx
@@ -14,7 +14,17 @@ const GpuServerDetail = ({ labId, ...rest }: GpuServerDetailProps) => {
     <div {...rest}>
       {data && (
         <>
-          <Text>GPU 연산량</Text>
+          <Text>서버 이름</Text>
+          <Text>{data.serverName}</Text>
+          <Text>서버 상태</Text>
+          <Text>{data.isOn ? "ON" : "OFF"}</Text>
+          <Text>RAM 용량</Text>
+          <Text>{data.memorySize}</Text>
+          <Text>디스크 용량</Text>
+          <Text>{data.diskSize}</Text>
+          <Text>GPU 장치명</Text>
+          <Text>{data.gpuBoard.modelName}</Text>
+          <Text>성능(TFLOPS)</Text>
           <Text>{data.gpuBoard.performance}</Text>
           <Text>현재 실행중인 Job</Text>
           <Text>{getCurrentJob(data)?.name ?? "N/A"}</Text>
