59c6e992ac1e20e8c05cb5be821469332a39d064
bigsaigon333 <likepeppermint@gmail.com>
2021-08-06 11:15:09 +0900

    refactor: axios fetch 함수 간소화

--- front-end/src/utils/axios.ts
+++ front-end/src/utils/axios.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { SESSION_STORAGE_KEY } from "../constants";
 import { RequestConfig } from "../types";
 
@@ -13,13 +13,11 @@ axios.interceptors.request.use((config) => {
   return config;
 });
 
-const fetch = <T>(url: string, { method, data }: AxiosRequestConfig) =>
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  axios(url, { method, data }).then((response) => response.data as T);
-
 // eslint-disable-next-line import/prefer-default-export
-export const getData = <T = void, U = never>(url: string, config?: RequestConfig<U>) => {
+export const getData = async <T = void, U = never>(url: string, config?: RequestConfig<U>) => {
   const { method = "get", body } = config ?? {};
 
-  return fetch<T>(url, { method, data: body });
+  const response = await axios(url, { method, data: body });
+
+  return response.data as T;
 };
