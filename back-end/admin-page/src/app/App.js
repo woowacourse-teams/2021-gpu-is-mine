0bf7d24775dc11fc9c6e75a3e117f4d88b683706
Naeun <knae11@naver.com>
2021-08-07 20:56:17 +0900

    chore: react 기본셋팅

---
+++ back-end/admin-page/src/app/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>hi</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ back-end/admin-page/src/index.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./app/App";
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
