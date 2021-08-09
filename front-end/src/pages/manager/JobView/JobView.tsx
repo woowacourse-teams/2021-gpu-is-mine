6bad194805a09eed00a44880638e70aec07bf250
bigsaigon333 <likepeppermint@gmail.com>
2021-08-09 16:52:20 +0900

    feat: labId, memberId, memberType을 propDrilling

--- front-end/src/pages/manager/JobView/JobView.tsx
+++ front-end/src/pages/manager/JobView/JobView.tsx
@@ -1,14 +1,23 @@
+import { useMyInfo } from "../../../hooks";
 import { Layout } from "../../../components";
 import { ManagerNavigation, ManagerHeader, ManagerSubHeader } from "../../../domains/Manager";
 import { JobInfoList } from "../../../domains/Job";
 
-const JobView = () => (
-  <Layout
-    Header={<ManagerHeader />}
-    SubHeader={<ManagerSubHeader />}
-    Navigation={<ManagerNavigation />}
-    Content={<JobInfoList />}
-  />
-);
+const JobView = () => {
+  const {
+    labResponse: { id: labId },
+    id: memberId,
+    memberType,
+  } = useMyInfo();
+
+  return (
+    <Layout
+      Header={<ManagerHeader />}
+      SubHeader={<ManagerSubHeader />}
+      Navigation={<ManagerNavigation />}
+      Content={<JobInfoList labId={labId} memberId={memberId} memberType={memberType} />}
+    />
+  );
+};
 
 export default JobView;
