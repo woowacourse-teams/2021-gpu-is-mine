4663d656d7f00804779819e711ece4922a3793d2
naeunkim <knae11@naver.com>
2021-08-10 16:31:10 +0900

    chore: 설정파일 분리

--- back-end/src/main/java/mine/is/gpu/config/ElasticClientConfig.java
+++ back-end/src/main/java/mine/is/gpu/config/ElasticClientConfig.java
@@ -1,22 +1,32 @@
 package mine.is.gpu.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
 @Configuration
+@PropertySource(value = {"classpath:application-elk.properties"})
 public class ElasticClientConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+    @Value("${elasticsearch.username}")
+    private String userName;
+    @Value("${elasticsearch.password}")
+    private String password;
 
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
 
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("52.79.250.91:9000")
-                .withBasicAuth("elastic", "changeme")
+                .connectedTo(host)
+                .withBasicAuth(userName, password)
                 .build();
 
         return RestClients.create(clientConfiguration).rest();
