e52609f2eaaafece59069cab0c1f132c6d603c80
bigsaigon333 <likepeppermint@gmail.com>
2021-08-11 22:53:04 +0900

    feat: Job 로그를 개행하여 보여준다

--- front-end/src/domains/JobDetailLogContent/JobDetailLogContent.tsx
+++ front-end/src/domains/JobDetailLogContent/JobDetailLogContent.tsx
@@ -12,11 +12,15 @@ const JobDetailLogContent = ({ logs, ...rest }: JobDetailLogContentProps) => {
 
   return (
     <ol {...rest}>
-      {logs.map((line) => (
-        <Text as="li" size="sm" key={line}>
-          {line}
-        </Text>
-      ))}
+      {logs
+        .map((line) => line.replace(/(\n\r|\r)/g, "\n").split("\n"))
+        .flat()
+        .map((line, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Text as="li" size="sm" key={`${line}_${index}`}>
+            {line}
+          </Text>
+        ))}
     </ol>
   );
 };
