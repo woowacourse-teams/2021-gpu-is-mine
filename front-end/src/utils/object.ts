fb5f0adfd83ce3e84eaebb60419511c811a350a3
2sooy <1401660@naver.com>
2021-08-11 23:48:49 +0900

    refactor: pick 유틸 함수 reduce 로직 수정

--- front-end/src/utils/object.ts
+++ front-end/src/utils/object.ts
@@ -2,8 +2,4 @@ type DataObject = Record<string, unknown>;
 
 // eslint-disable-next-line import/prefer-default-export
 export const pick = (obj: DataObject, properties: string[]) =>
-  properties.reduce((result, prop) => {
-    // eslint-disable-next-line no-param-reassign
-    result[prop] = obj[prop];
-    return result;
-  }, {} as DataObject);
+  properties.reduce((result, prop) => ({ ...result, [prop]: obj[prop] }), {} as DataObject);
