b080ef863d4d5f2d844f1420e73a376b98729eec
naeunkim <knae11@naver.com>
2021-08-11 15:38:20 +0900

    chore: prod 배포시 초기화 정보 설정

--- back-end/src/main/java/mine/is/gpu/AdminDataLoader.java
+++ back-end/src/main/java/mine/is/gpu/AdminDataLoader.java
@@ -3,15 +3,23 @@ package mine.is.gpu;
 import mine.is.gpu.admin.Administrator;
 import mine.is.gpu.admin.AdministratorRepository;
 import mine.is.gpu.encryption.Encrypt;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-@Profile("test")
+@PropertySource(value = {"classpath:application-db.properties"})
+@Profile({"was1", "was2"})
 @Component
 public class AdminDataLoader implements CommandLineRunner {
     private final AdministratorRepository administratorRepository;
     private final Encrypt encrypt;
+
+    @Value("${db.admin.email}")
+    public String prodEmail;
+    @Value("${db.admin.password}")
+    public String prodPassword;
 
     public static final String ADMIN_EMAIL = "admin@email.com";
     public static final String ADMIN_PASSWORD = "password";
@@ -23,8 +31,8 @@ public class AdminDataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        final String password = encrypt.hashedPassword(ADMIN_PASSWORD, ADMIN_EMAIL);
-        Administrator administrator = new Administrator(ADMIN_EMAIL, password);
+        final String password = encrypt.hashedPassword(prodPassword, prodEmail);
+        Administrator administrator = new Administrator(prodEmail, password);
         administratorRepository.save(administrator);
     }
 }
--- back-end/src/main/java/mine/is/gpu/DataLoader.java
+++ back-end/src/main/java/mine/is/gpu/DataLoader.java
@@ -13,12 +13,15 @@ import mine.is.gpu.member.application.MemberService;
 import mine.is.gpu.member.domain.Member;
 import mine.is.gpu.member.domain.repository.MemberRepository;
 import mine.is.gpu.member.dto.request.MemberRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Profile("local")
+@PropertySource(value = {"classpath:application-db.properties"})
+@Profile({"was1", "was2"})
 @Component
 public class DataLoader implements CommandLineRunner {
     private final LabRepository labRepository;
@@ -28,6 +31,11 @@ public class DataLoader implements CommandLineRunner {
     private final JobRepository jobRepository;
 
     private final MemberService memberService;
+
+    @Value("${db.member.email}")
+    private String prodMemberEmail;
+    @Value("${db.member.password}")
+    private String prodMemberPassword;
 
     public DataLoader(LabRepository labRepository,
                       GpuServerRepository gpuServerRepository,
@@ -72,51 +80,11 @@ public class DataLoader implements CommandLineRunner {
         gpuBoardRepository.save(gpuBoard4);
 
         Long savedMemberId1 = memberService
-                .save(new MemberRequest("email@email.com", "password", "name", "manager", lab1.getId()));
+                .save(new MemberRequest(prodMemberEmail, prodMemberPassword, "name", "manager", lab1.getId()));
         Member member1 = memberRepository.findById(savedMemberId1).get();
-
-        Long savedMemberId2 = memberService
-                .save(new MemberRequest("test@test.com", "test1234!", "name", "manager", lab1.getId()));
-        Member member2 = memberRepository.findById(savedMemberId2).get();
 
         Job job1 = new Job("영화 리뷰 분석을 통한 긍정도 평가", JobStatus.WAITING, gpuBoard1, member1,
                 "danny/movie_review", "80");
         jobRepository.save(job1);
-
-        Job job2 = new Job("가짜 뉴스 검증을 위한 댓글 분류 학습", JobStatus.RUNNING, gpuBoard1, member1,
-                "myagya/fake_news", "50");
-        jobRepository.save(job2);
-
-        Job job3 = new Job("신경망을 이용한 스포츠 경기 비디오와 텍스트 요약", JobStatus.COMPLETED, gpuBoard1, member1,
-                "better/sports_analysis", "60");
-        jobRepository.save(job3);
-
-        Job job4 = new Job("보스턴 주택 가격 예측과 k-겹 검증", JobStatus.COMPLETED, gpuBoard1, member1,
-                "ed/housing_prices", "70");
-        jobRepository.save(job4);
-
-        Job job5 = new Job("소셜 미디어 게시물을 기반으로 한 우울증 감정 분석", JobStatus.WAITING, gpuBoard1, member2,
-                "wannte/social_media", "20");
-        jobRepository.save(job5);
-
-        Job job6 = new Job("교통 표지판 분류 학습", JobStatus.CANCELED, gpuBoard2, member2,
-                "corgy/traffic_signs", "30");
-        jobRepository.save(job6);
-
-        Job job7 = new Job("CNN 모델을 활용한 마스크 인식 학습", JobStatus.WAITING, gpuBoard2, member1,
-                "collin/mask_recognition", "50");
-        jobRepository.save(job7);
-
-        Job job8 = new Job("자연어 처리 : 문자-단위 RNN으로 이름 생성하기", JobStatus.RUNNING, gpuBoard3, member1,
-                "danny/natural_language", "100");
-        jobRepository.save(job8);
-
-        Job job9 = new Job("유튜브 댓글 긍정도 평가 학습", JobStatus.WAITING, gpuBoard4, member2,
-                "myagya/youtube_comments", "20");
-        jobRepository.save(job9);
-
-        Job job10 = new Job("망막 이미지 분류, 망막 질환 진단 학습", JobStatus.RUNNING, gpuBoard4, member2,
-                "better/retina_classification", "10");
-        jobRepository.save(job10);
     }
 }
