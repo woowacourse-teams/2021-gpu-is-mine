eaed94921f2a65a8515962fbc448371ffaba1579
bigsaigon333 <likepeppermint@gmail.com>
2021-08-11 16:15:37 +0900

    feat: API 호출을 통해서 Job 파싱된 로그를 가져와 차트를 그린다

--- front-end/src/domains/JobDetailGraph/JobDetailGraph.tsx
+++ front-end/src/domains/JobDetailGraph/JobDetailGraph.tsx
@@ -1,4 +1,6 @@
-import { Text } from "../../components";
+import { useEffect } from "react";
+import { useGetJobDetailLogForGraph } from "../../hooks/useApi/useApi";
+import { Text, Loading } from "../../components";
 import JobDetailGraphChart from "../JobDetailGraphChart/JobDetailGraphChart";
 import { StyledJobDetailGraph } from "./JobDetailGraph.styled";
 import { JobViewResponse } from "../../types";
@@ -6,15 +8,29 @@ import { JobViewResponse } from "../../types";
 interface JobDetailGraphProps {
   className?: string;
   detail: JobViewResponse;
+  jobId: number;
+  labId: number;
 }
 
-const JobDetailGraph = ({ detail, ...rest }: JobDetailGraphProps) => (
-  <StyledJobDetailGraph {...rest}>
-    <Text as="h3" weight="bold" size="lg">
-      그래프
-    </Text>
-    <JobDetailGraphChart data={[]} />
-  </StyledJobDetailGraph>
-);
+const JobDetailGraph = ({ labId, jobId, detail, ...rest }: JobDetailGraphProps) => {
+  const { data, makeRequest, isLoading, done } = useGetJobDetailLogForGraph({ labId, jobId });
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    makeRequest();
+
+    return done;
+  }, [makeRequest, done]);
+
+  return (
+    <StyledJobDetailGraph {...rest}>
+      <Text as="h3" weight="bold" size="lg">
+        그래프
+      </Text>
+      {isLoading && <Loading />}
+      <JobDetailGraphChart data={data?.parsedLogResponses ?? []} />
+    </StyledJobDetailGraph>
+  );
+};
 
 export default JobDetailGraph;
