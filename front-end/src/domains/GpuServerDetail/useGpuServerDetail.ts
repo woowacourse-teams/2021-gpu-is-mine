496cd499b201f9089d1a964403ad60b9e76ec7a9
bigsaigon333 <likepeppermint@gmail.com>
2021-08-12 15:49:54 +0900

    feat: serverId 가 number 임을 보장한다

--- front-end/src/domains/GpuServerDetail/useGpuServerDetail.ts
+++ front-end/src/domains/GpuServerDetail/useGpuServerDetail.ts
@@ -8,7 +8,15 @@ export const getCurrentJob = (server: GpuServer): JobViewResponse | undefined =>
 export const getWaitingJob = (server: GpuServer): JobViewResponse[] =>
   server.jobs.filter((job) => job.status === "WAITING");
 
-export const useServerId = () => Number(useParseParams("serverId"));
+export const useServerId = () => {
+  const serverId = useParseParams("serverId");
+
+  if (Number.isNaN(Number(serverId))) {
+    throw Error(`Invalid serverId in params: ${serverId}`);
+  }
+
+  return Number(serverId);
+};
 
 export const useGpuServerDetail = ({ labId, serverId }: { labId: number; serverId: number }) => {
   const { data, makeRequest, done, ...rest } = useGetGpuServerById({ labId, serverId });
