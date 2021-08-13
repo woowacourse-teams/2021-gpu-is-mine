04ce1446e6448c040c1a4a435e2c6f02e0fabae2
Danny Kim <likepeppermint@gmail.com>
2021-08-13 11:50:30 +0900

    feat: 학습중인 Job 로그 데이터의 자동갱신 기능을 구현한다 (#321)
    * feat: useInterval 구현
    
    * feat: delay가 null일 때는 interval을 실행하지 않는다
    
    * feat: API call을 주기적으로 실행할 시간 단위를 상수화한다
    
    * feat: useInterval re-export
    
    * refactor: GpuServer 상세조회 페이지에서 jobId를 prop 으로 넘겨받는다
    
    * feat: Job 상세조회 로그와 그래프에서 주기적으로 api call 을 호출한다
    
    * feat: Job 이 waiting 일  때도 주기적으로 API 를 호출한다

--- front-end/src/constants/index.ts
+++ front-end/src/constants/index.ts
@@ -2,3 +2,4 @@ export { default as PATH, Route } from "./path";
 export { default as API_ENDPOINT, BASE_URL } from "./api";
 export { default as MESSAGE } from "./message";
 export * from "./storageKey";
+export * from "./delay";
--- front-end/src/domains/GpuServerDetailCurrentJob/GpuServerDetailCurrentJob.tsx
+++ front-end/src/domains/GpuServerDetailCurrentJob/GpuServerDetailCurrentJob.tsx
@@ -1,20 +1,19 @@
 import { GpuServer } from "../../types";
-import { getCurrentJob } from "../GpuServerDetailSummary/useGpuServerDetailSummary";
 import { useJobDetail } from "../JobDetail/useJobDetail";
 import JobDetailSummary from "../JobDetailSummary/JobDetailSummary";
 
 interface GpuServerDetailCurrentJobProps {
   className?: string;
   labId: number;
+  jobId: number;
   detail: GpuServer;
 }
 const GpuServerDetailCurrentJob = ({
   labId,
   detail: gpuServerDetail,
+  jobId,
   ...rest
 }: GpuServerDetailCurrentJobProps) => {
-  const jobId = getCurrentJob(gpuServerDetail)?.id ?? -1;
-
   const { detail: jobDetail } = useJobDetail({ labId, jobId });
 
   return <div {...rest}>{jobDetail && <JobDetailSummary detail={jobDetail} />}</div>;
