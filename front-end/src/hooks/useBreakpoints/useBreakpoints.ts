4694ae581b2dd6f004d7afafd5d308cf24aaa320
bigsaigon333 <likepeppermint@gmail.com>
2021-08-18 21:06:21 +0900

    refactor: useBreakpoints 반환값의 타입을 boolean으로 일원화한다

--- front-end/src/hooks/useBreakpoints/useBreakpoints.ts
+++ front-end/src/hooks/useBreakpoints/useBreakpoints.ts
@@ -1,10 +1,13 @@
 import { only, up } from "styled-breakpoints";
 import { useBreakpoint } from "styled-breakpoints/react-styled";
 
+/**
+ * 현재 뷰포트를 나타내는 boolean flag를 반환한다
+ */
 const useBreakpoints = () => {
-  const isMobile = useBreakpoint(only("mobile"));
-  const isTablet = useBreakpoint(only("tablet"));
-  const isLaptop = useBreakpoint(up("laptop"));
+  const isMobile = useBreakpoint(only("mobile")) ?? false;
+  const isTablet = useBreakpoint(only("tablet")) ?? false;
+  const isLaptop = useBreakpoint(up("laptop")) ?? false;
 
   return {
     isMobile,
