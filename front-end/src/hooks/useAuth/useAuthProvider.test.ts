09bda6c8c0234be769c069cb1f7446f8a3dfa689
bigsaigon333 <likepeppermint@gmail.com>
2021-08-27 15:55:48 +0900

    test(useAuthProvider): 불필요한 mock을 제거한다

--- front-end/src/hooks/useAuth/useAuthProvider.test.ts
+++ front-end/src/hooks/useAuth/useAuthProvider.test.ts
@@ -1,25 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { renderHook, act } from "@testing-library/react-hooks/dom";
 import { rest } from "msw";
 import useAuthProvider from "./useAuthProvider";
 import { API_ENDPOINT, SESSION_STORAGE_KEY } from "../../constants";
 import server from "../../__mocks__/server";
-
-const mockRequestSignup = jest.fn();
-
-jest.mock("../useApi/useApi", () => {
-  const originalUseApi = jest.requireActual("../useApi/useApi");
-
-  return {
-    ...originalUseApi,
-    usePostSignup: () => ({
-      ...originalUseApi.usePostSignup,
-      makeRequest: mockRequestSignup,
-    }),
-  };
-});
 
 describe("useAuthProvider", () => {
   afterEach(() => {
@@ -62,23 +45,6 @@ describe("useAuthProvider", () => {
     expect(result.current.isAuthenticated).toBe(false);
 
     expect(sessionStorage.getItem(SESSION_STORAGE_KEY.ACCESS_TOKEN)).toBeNull();
-  });
-
-  test("signup을 호출하면 signup API를 호출한다", async () => {
-    const { result } = renderHook(() => useAuthProvider());
-
-    act(() => {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      result.current.signup({
-        email: "abcd@abcd.com",
-        labId: 1,
-        memberType: "USER",
-        name: "dd",
-        password: "abcd123!@#",
-      });
-    });
-
-    expect(mockRequestSignup).toHaveBeenCalled();
   });
 
   describe("sessionStorage에 token이 존재하는 경우", () => {
