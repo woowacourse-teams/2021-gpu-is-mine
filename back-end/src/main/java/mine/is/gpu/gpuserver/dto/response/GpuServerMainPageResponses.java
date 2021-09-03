e314b48f7496cdbea65980b0609b65c9d2e6ee24
JinHwanKim <ecsimsw@gmail.com>
2021-09-03 22:19:50 +0900

    feat : 전체 검색 시 노출되는 데이터, 쿼리 축소 (#352)
    * feat : 전체 검색 시 노출되는 데이터, 쿼리 축소
    
    * refactor : 피드백 반영 / 클래스명 수정

---
+++ back-end/src/main/java/mine/is/gpu/gpuserver/dto/response/GpuServerMainPageResponses.java
@@ -0,0 +1,19 @@
+package mine.is.gpu.gpuserver.dto.response;
+
+import java.util.List;
+
+public class GpuServerMainPageResponses {
+    private List<GpuServerMainPageResponse> gpuServerMainPageRespons;
+
+    private GpuServerMainPageResponses(List<GpuServerMainPageResponse> gpuServerMainPageRespons) {
+        this.gpuServerMainPageRespons = gpuServerMainPageRespons;
+    }
+
+    public static GpuServerMainPageResponses of(List<GpuServerMainPageResponse> gpus) {
+        return new GpuServerMainPageResponses(gpus);
+    }
+
+    public List<GpuServerMainPageResponse> getGpuServers() {
+        return gpuServerMainPageRespons;
+    }
+}
