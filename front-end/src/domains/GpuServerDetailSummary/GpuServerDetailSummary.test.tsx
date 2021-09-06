227700bafd0c4399ec6f7ba672ad18073860ff58
Danny Kim <likepeppermint@gmail.com>
2021-09-06 11:16:30 +0900

    fix: gpuServer 전체 검색시 데이터, 쿼리 축소에 대응한다 (#360)

--- front-end/src/domains/GpuServerDetailSummary/GpuServerDetailSummary.test.tsx
+++ front-end/src/domains/GpuServerDetailSummary/GpuServerDetailSummary.test.tsx
@@ -2,28 +2,32 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { render, screen } from "@testing-library/react";
 import GpuServerDetailSummary from "./GpuServerDetailSummary";
-import { gpuServerResponse } from "../../__fixtures__";
+import { gpuServerDetailResponse } from "../../__fixtures__";
 
 describe("GpuServerDetailSummary", () => {
   test("data", () => {
-    render(<GpuServerDetailSummary detail={gpuServerResponse} serverId={1} labId={1} />);
+    render(<GpuServerDetailSummary detail={gpuServerDetailResponse} serverId={1} labId={1} />);
 
-    expect(screen.getByText(gpuServerResponse.serverName)).toBeInTheDocument();
+    expect(screen.getByText(gpuServerDetailResponse.serverName)).toBeInTheDocument();
 
     expect(screen.getByRole("alert", { name: /서버 상태/ })).toBeInTheDocument();
 
     expect(screen.getByText(/RAM 용량/)).toBeInTheDocument();
-    expect(screen.getByText(gpuServerResponse.memorySize, { exact: false })).toBeInTheDocument();
+    expect(
+      screen.getByText(gpuServerDetailResponse.memorySize, { exact: false })
+    ).toBeInTheDocument();
 
     expect(screen.getByText(/디스크 용량/)).toBeInTheDocument();
-    expect(screen.getByText(gpuServerResponse.diskSize, { exact: false })).toBeInTheDocument();
+    expect(
+      screen.getByText(gpuServerDetailResponse.diskSize, { exact: false })
+    ).toBeInTheDocument();
 
     expect(screen.getByText(/GPU 장치명/)).toBeInTheDocument();
-    expect(screen.getByText(gpuServerResponse.gpuBoard.modelName)).toBeInTheDocument();
+    expect(screen.getByText(gpuServerDetailResponse.gpuBoard.modelName)).toBeInTheDocument();
 
     expect(screen.getByText(/성능/)).toBeInTheDocument();
     expect(
-      screen.getByText(gpuServerResponse.gpuBoard.performance, { exact: false })
+      screen.getByText(gpuServerDetailResponse.gpuBoard.performance, { exact: false })
     ).toBeInTheDocument();
 
     expect(screen.getByText(/현재 실행중인 Job/)).toBeInTheDocument();
