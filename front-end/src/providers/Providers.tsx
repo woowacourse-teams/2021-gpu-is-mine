90bdb0bda05c7095b0f80cbb6c5ffe5a61b8a0d1
bigsaigon333 <likepeppermint@gmail.com>
2021-09-28 16:57:26 +0900

    feat: Provider에 store를 추가한다

--- front-end/src/providers/Providers.tsx
+++ front-end/src/providers/Providers.tsx
@@ -1,19 +1,23 @@
 import { ReactNode } from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "../components";
 import { GlobalStyle, ThemeProvider } from "../styles";
+import { store } from "../app/store";
 
 interface ProvidersProps {
   children?: ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps): JSX.Element => (
-  <ThemeProvider>
-    <GlobalStyle />
-    <AuthProvider>
-      <Router>{children}</Router>
-    </AuthProvider>
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider>
+      <GlobalStyle />
+      <AuthProvider>
+        <Router>{children}</Router>
+      </AuthProvider>
+    </ThemeProvider>
+  </Provider>
 );
 
 export default Providers;
