8c319ba043b26266611bfc3ab99618f82c935aa8
bigsaigon333 <likepeppermint@gmail.com>
2021-09-28 16:56:32 +0900

    feat: store에 memberReducer를 추가한다

--- front-end/src/app/store.ts
+++ front-end/src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import memberReducer from "../features/member/memberSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    member: memberReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
