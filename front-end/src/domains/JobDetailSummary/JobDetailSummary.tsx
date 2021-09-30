4cd04e58ee20da551c6958d7c8170241b9751708
2sooy <1401660@naver.com>
2021-09-30 21:27:51 +0900

    feat: gpu 서버, job 상세 조회 페이지 마크업 개선
    - h4 제거
    - dl > dt + dd 구조 변경

--- front-end/src/domains/JobDetailSummary/JobDetailSummary.tsx
+++ front-end/src/domains/JobDetailSummary/JobDetailSummary.tsx
@@ -1,5 +1,9 @@
 import { Text } from "../../components";
-import { StyledJobDetailSummary, Anchor } from "./JobDetailSummary.styled";
+import {
+  StyledJobDetailSummary,
+  StyledJobDetailSummaryList,
+  Anchor,
+} from "./JobDetailSummary.styled";
 import { JobViewResponse } from "../../types";
 
 interface JobDetailSummaryProps {
@@ -13,48 +17,38 @@ const JobDetailSummary = ({ detail, ...rest }: JobDetailSummaryProps) => {
   return (
     <StyledJobDetailSummary {...rest}>
       <Text as="h3" weight="bold" size="lg">
-        요약 정보
+        {detail.name}
       </Text>
-      <div>
-        <Text as="h4" weight="bold">
-          Job 이름
-        </Text>
-        <Text>{detail.name}</Text>
-      </div>
-      <div>
-        <Text as="h4" weight="bold">
+      <StyledJobDetailSummaryList>
+        <Text as="dt" weight="bold">
           Job 상태
         </Text>
-        <Text>{detail.status}</Text>
-      </div>
-      <div>
-        <Text as="h4" weight="bold">
+        <Text as="dd">{detail.status}</Text>
+
+        <Text as="dt" weight="bold">
           Job 등록자
         </Text>
-        <Text>{detail.memberName}</Text>
-      </div>
-      <div>
-        <Text as="h4" weight="bold">
+        <Text as="dd">{detail.memberName}</Text>
+
+        <Text as="dt" weight="bold">
           할당된 서버
         </Text>
-        <Text>{detail.gpuServerName}</Text>
-      </div>
-      <div>
-        <Text as="h4" weight="bold">
+        <Text as="dd">{detail.gpuServerName}</Text>
+
+        <Text as="dt" weight="bold">
           {detail.status === "RUNNING" ? "" : "예상 "}실행시간
         </Text>
-        <Text>{detail.expectedTime}</Text>
-      </div>
-      <div>
-        <Text as="h4" weight="bold">
+        <Text as="dd">{detail.expectedTime}</Text>
+
+        <Text as="dt" weight="bold">
           DockerHub URL
         </Text>
-        <Text>
+        <Text as="dd">
           <Anchor target="_blank" href={url} rel="noreferrer">
             {url}
           </Anchor>
         </Text>
-      </div>
+      </StyledJobDetailSummaryList>
     </StyledJobDetailSummary>
   );
 };
