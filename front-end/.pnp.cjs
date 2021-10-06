49874c255845e7e621cc35fadc6e5c22c6afb5d2
Danny Kim <likepeppermint@gmail.com>
2021-10-06 17:29:00 +0900

    chore: 환경변수를 .env 파일로 주입한다 (#419)
    * chore: dotenv에서 BASE_URL을 주입한다
    
    * chore: test 환경에서 dotenv를 적용한다
    
    * chore: CI 환경에서 .env 파일을 생성한다
    
    * fix: CI 환경에서 .env 파일을 생성한다
    
    * test: BASE_URL  테스트
    
    * chore: yarn install을 추가한다
    
    * fix: front-end 디렉토리에서 .env 파일을 생성한다
    
    * chore: BASE_URL 환경변수를 최상단으로 끌어올린다
    
    * fix: FE-build 시 working directory에서 env를 생성한다

--- front-end/.pnp.cjs
+++ front-end/.pnp.cjs
@@ -49,6 +49,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@testing-library/react", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:12.0.0"],
             ["@testing-library/react-hooks", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:7.0.1"],
             ["@testing-library/user-event", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:13.2.1"],
+            ["@types/dotenv", "npm:8.2.0"],
             ["@types/jest", "npm:26.0.24"],
             ["@types/node", "npm:16.4.13"],
             ["@types/react", "npm:17.0.13"],
@@ -65,6 +66,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["classnames", "npm:2.3.1"],
             ["cypress", "npm:8.3.1"],
             ["debug", "virtual:904b3fada4a3265c4dfeed8c42d6542b5b497582d17441c86eea102109bfc3b9e1825aad904bc43dd1b94603ff4820b8cb2ebcf24cfbaf45ecee04e5bc1076b8#npm:4.3.2"],
+            ["dotenv", "npm:10.0.0"],
+            ["dotenv-webpack", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:7.0.3"],
             ["eslint", "npm:7.30.0"],
             ["eslint-config-airbnb", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:18.2.1"],
             ["eslint-config-airbnb-typescript", "npm:12.3.1"],
@@ -7854,6 +7857,16 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
+      ["@types/dotenv", [
+        ["npm:8.2.0", {
+          "packageLocation": "./.yarn/cache/@types-dotenv-npm-8.2.0-f4d0e3d65b-a1f524da7d.zip/node_modules/@types/dotenv/",
+          "packageDependencies": [
+            ["@types/dotenv", "npm:8.2.0"],
+            ["dotenv", "npm:10.0.0"]
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
       ["@types/eslint", [
         ["npm:7.2.13", {
           "packageLocation": "./.yarn/cache/@types-eslint-npm-7.2.13-37f10d8e02-6dac9f9d7d.zip/node_modules/@types/eslint/",
@@ -13053,6 +13066,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["dotenv", [
+        ["npm:10.0.0", {
+          "packageLocation": "./.yarn/cache/dotenv-npm-10.0.0-36b272df25-f412c5fe8c.zip/node_modules/dotenv/",
+          "packageDependencies": [
+            ["dotenv", "npm:10.0.0"]
+          ],
+          "linkType": "HARD",
+        }],
         ["npm:6.2.0", {
           "packageLocation": "./.yarn/cache/dotenv-npm-6.2.0-72b7dafc27-d4aa189741.zip/node_modules/dotenv/",
           "packageDependencies": [
@@ -13076,6 +13096,14 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["dotenv", "npm:6.2.0"]
           ],
           "linkType": "HARD",
+        }],
+        ["npm:2.0.2", {
+          "packageLocation": "./.yarn/cache/dotenv-defaults-npm-2.0.2-436a76242a-c005960bd0.zip/node_modules/dotenv-defaults/",
+          "packageDependencies": [
+            ["dotenv-defaults", "npm:2.0.2"],
+            ["dotenv", "npm:8.6.0"]
+          ],
+          "linkType": "HARD",
         }]
       ]],
       ["dotenv-expand", [
@@ -13095,6 +13123,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],
           "linkType": "SOFT",
         }],
+        ["npm:7.0.3", {
+          "packageLocation": "./.yarn/cache/dotenv-webpack-npm-7.0.3-94dd693fe9-d80dcbebe3.zip/node_modules/dotenv-webpack/",
+          "packageDependencies": [
+            ["dotenv-webpack", "npm:7.0.3"]
+          ],
+          "linkType": "SOFT",
+        }],
         ["virtual:01e2e6f82e4eeb112434f8140fdfb91d29afcd7ceff8a9c3bd500405b53c9c81d4044d74afb6d44b55975311cbf1362bdd9dd61788b3c48eae3846eaff60a55c#npm:1.8.0", {
           "packageLocation": "./.yarn/__virtual__/dotenv-webpack-virtual-6f2b4e95fa/0/cache/dotenv-webpack-npm-1.8.0-60ebc8f6b9-21bfe5dd6a.zip/node_modules/dotenv-webpack/",
           "packageDependencies": [
@@ -13102,6 +13137,20 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/webpack", "npm:4.41.30"],
             ["dotenv-defaults", "npm:1.1.1"],
             ["webpack", "virtual:01e2e6f82e4eeb112434f8140fdfb91d29afcd7ceff8a9c3bd500405b53c9c81d4044d74afb6d44b55975311cbf1362bdd9dd61788b3c48eae3846eaff60a55c#npm:4.46.0"]
+          ],
+          "packagePeers": [
+            "@types/webpack",
+            "webpack"
+          ],
+          "linkType": "HARD",
+        }],
+        ["virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:7.0.3", {
+          "packageLocation": "./.yarn/__virtual__/dotenv-webpack-virtual-fc1815f42a/0/cache/dotenv-webpack-npm-7.0.3-94dd693fe9-d80dcbebe3.zip/node_modules/dotenv-webpack/",
+          "packageDependencies": [
+            ["dotenv-webpack", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:7.0.3"],
+            ["@types/webpack", null],
+            ["dotenv-defaults", "npm:2.0.2"],
+            ["webpack", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:5.42.0"]
           ],
           "packagePeers": [
             "@types/webpack",
@@ -15580,6 +15629,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@testing-library/react", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:12.0.0"],
             ["@testing-library/react-hooks", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:7.0.1"],
             ["@testing-library/user-event", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:13.2.1"],
+            ["@types/dotenv", "npm:8.2.0"],
             ["@types/jest", "npm:26.0.24"],
             ["@types/node", "npm:16.4.13"],
             ["@types/react", "npm:17.0.13"],
@@ -15596,6 +15646,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["classnames", "npm:2.3.1"],
             ["cypress", "npm:8.3.1"],
             ["debug", "virtual:904b3fada4a3265c4dfeed8c42d6542b5b497582d17441c86eea102109bfc3b9e1825aad904bc43dd1b94603ff4820b8cb2ebcf24cfbaf45ecee04e5bc1076b8#npm:4.3.2"],
+            ["dotenv", "npm:10.0.0"],
+            ["dotenv-webpack", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:7.0.3"],
             ["eslint", "npm:7.30.0"],
             ["eslint-config-airbnb", "virtual:e4837ad80ea865654442f4ae7802500e03888811f630592b221e7fff9c3e1faa7dcc861fb157dc5bf3bf362aa12cda3c0f7783ee1cd0fb48df904d4f54ae7ca4#npm:18.2.1"],
             ["eslint-config-airbnb-typescript", "npm:12.3.1"],
