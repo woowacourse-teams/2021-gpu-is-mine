b6974d89289ff7732840480c64aa2c69beabbe45
bigsaigon333 <likepeppermint@gmail.com>
2021-10-06 14:39:15 +0900

    chore: dev server API endpoint 변경을 반영한다

--- front-end/src/constants/api.ts
+++ front-end/src/constants/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.BASE_URL ?? "https://gpuismine.kro.kr/api"; // dev server;
+export const BASE_URL = process.env.BASE_URL ?? "https://dev-api.gpuismine.com/api"; // dev server;
 
 const API_ENDPOINT = {
   LABS(labId: number) {
