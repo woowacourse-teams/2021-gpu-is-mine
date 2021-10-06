7a452f92697dfa2687fe145a2c14db7f23377c4e
Danny Kim <likepeppermint@gmail.com>
2021-10-06 17:29:00 +0900

    chore: 환경변수를 .env 파일로 주입한다 (#419)
    * chore: dotenv에서 BASE_URL을 주입한다
    
    * chore: test 환경에서 dotenv를 적용한다
    
    * chore: CI 환경에서 .env 파일을 생성한다
    
    * fix: CI 환경에서 .env 파일을 생성한다
    
    * test: BASE_URL  테스트
    
    * chore: yarn install을 추가한다
    
    * fix: front-end 디렉토리에서 .env 파일을 생성한다
    
    * chore: BASE_URL 환경변수를 최상단으로 끌어올린다
    
    * fix: FE-build 시 working directory에서 env를 생성한다

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -8,4 +8,5 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.ts",
   },
+  globalSetup: "<rootDir>/__test__/dotenv.ts",
 };
