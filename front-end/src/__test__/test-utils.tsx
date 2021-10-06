d8d2fc2b4c2fd1569b8f2146fbab45cd202d928a
bigsaigon333 <likepeppermint@gmail.com>
2021-10-06 22:49:51 +0900

    test: Redux test 시 store를 외부에서 주입 가능한 customRender를 구현한다

--- front-end/src/__test__/test-utils.tsx
+++ front-end/src/__test__/test-utils.tsx
@@ -1,9 +1,41 @@
-import { ReactElement } from "react";
+import { ComponentType, ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
-import Providers from "../providers/Providers";
+import { configureStore, EnhancedStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import signupReducer from "../features/member/signupSlice";
+import authReducer from "../features/member/authSlice";
+import { ThemeProvider } from "../styles";
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
-  render(ui, { wrapper: Providers, ...options });
+type CustomRender = (
+  ui: ReactElement,
+  {
+    store,
+    ...renderOptions
+  }?: {
+    store: EnhancedStore;
+    renderOptions?: Omit<RenderOptions, "wrapper">;
+  }
+) => ReturnType<typeof render>;
+
+const customRender: CustomRender = (
+  ui,
+  { store, ...renderOptions } = {
+    store: configureStore({
+      reducer: { auth: authReducer, signup: signupReducer },
+    }),
+  }
+) => {
+  const Wrapper: ComponentType = ({ children }) => (
+    <Provider store={store}>
+      <ThemeProvider>
+        <MemoryRouter>{children}</MemoryRouter>
+      </ThemeProvider>
+    </Provider>
+  );
+
+  return render(ui, { wrapper: Wrapper, ...renderOptions });
+};
 
 export * from "@testing-library/react";
 
