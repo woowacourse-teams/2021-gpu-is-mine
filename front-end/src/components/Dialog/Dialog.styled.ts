0741d47d7020d9b4aceb5b34e73a14ef082cc142
bigsaigon333 <likepeppermint@gmail.com>
2021-10-15 22:13:00 +0900

    feat: Dialog가 표시되면 확인과 취소 버튼만 Tab 키로 이동가능하다

--- front-end/src/components/Dialog/Dialog.styled.ts
+++ front-end/src/components/Dialog/Dialog.styled.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import Button from "../Button/Button";
 
 export const Inner = styled.dialog`
   display: grid;
@@ -16,6 +15,10 @@ export const Inner = styled.dialog`
 
   padding: 0;
   position: sticky;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 export const Body = styled.div`
@@ -25,7 +28,6 @@ export const Body = styled.div`
 
   word-break: break-all;
   padding: 0.5rem 2rem;
-  user-select: none;
 `;
 
 export const Footer = styled.div`
@@ -33,24 +35,38 @@ export const Footer = styled.div`
   flex-direction: row;
 
   font-size: 1rem;
+  user-select: none;
 `;
 
-export const CancelButton = styled(Button)`
+export const CancelButton = styled.button`
   color: var(--warning);
   background-color: inherit;
   font-weight: 500;
+  width: 100%;
 
-  &:hover {
+  &:hover,
+  &:focus {
     background-color: rgba(248, 101, 101, 0.1);
+    font-weight: 600;
+  }
+
+  &:focus {
+    outline: none;
   }
 `;
 
-export const ConfirmButton = styled(Button)`
+export const ConfirmButton = styled.button`
   color: var(--primary-800);
   background-color: inherit;
   font-weight: 500;
+  width: 100%;
 
-  &:hover {
+  &:hover,
+  &:focus {
     background-color: rgba(52, 67, 85, 0.1);
+  }
+
+  &:focus {
+    outline: none;
   }
 `;
--- front-end/src/components/Dialog/Dialog.tsx
+++ front-end/src/components/Dialog/Dialog.tsx
@@ -1,12 +1,12 @@
-import { ReactNode, MouseEventHandler, ComponentProps, HTMLAttributes } from "react";
+import { ReactNode, ComponentProps, HTMLAttributes, useRef, useEffect, useCallback } from "react";
 import Dimmer from "../Dimmer/Dimmer";
 import Portal from "../Portal/Portal";
 import { Inner, Body, Footer, CancelButton, ConfirmButton } from "./Dialog.styled";
 
 interface DialogProps extends HTMLAttributes<HTMLDialogElement> {
   dimmedColor?: ComponentProps<typeof Dimmer>["color"];
-  onConfirm: MouseEventHandler<HTMLButtonElement>;
-  onCancel?: MouseEventHandler<HTMLButtonElement>;
+  onConfirm: () => void;
+  onCancel?: () => void;
   children: ReactNode;
   className?: string;
 }
@@ -18,18 +18,61 @@ const Dialog = ({
   onCancel,
   className,
   ...rest
-}: DialogProps) => (
-  <Portal>
-    <Dimmer color={dimmedColor}>
-      <Inner className={className} aria-describedby="dialog-body" {...rest}>
-        <Body id="dialog-body">{children}</Body>
-        <Footer>
-          {onCancel ? <CancelButton onClick={onCancel}>취소</CancelButton> : null}
-          <ConfirmButton onClick={onConfirm}>확인</ConfirmButton>
-        </Footer>
-      </Inner>
-    </Dimmer>
-  </Portal>
-);
+}: DialogProps) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const cancelButtonRef = useRef<HTMLButtonElement>(null);
+  const confirmButtonRef = useRef<HTMLButtonElement>(null);
+  const indexRef = useRef(0);
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      const Buttons = [onCancel && cancelButtonRef.current, confirmButtonRef.current].filter(
+        Boolean
+      ) as HTMLElement[];
+
+      if (event.key === "Tab") {
+        event.preventDefault();
+        Buttons[indexRef.current].focus();
+        indexRef.current = (indexRef.current + 1) % Buttons.length;
+      }
+    },
+    [onCancel]
+  );
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    dialogRef.current!.focus();
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [handleKeyDown]);
+
+  return (
+    <Portal>
+      <Dimmer color={dimmedColor}>
+        <Inner
+          ref={dialogRef}
+          tabIndex={-1}
+          className={className}
+          aria-describedby="dialog-body"
+          {...rest}
+        >
+          <Body id="dialog-body">{children}</Body>
+          <Footer>
+            {onCancel ? (
+              <CancelButton ref={cancelButtonRef} onClick={onCancel}>
+                취소
+              </CancelButton>
+            ) : null}
+            <ConfirmButton ref={confirmButtonRef} onClick={onConfirm}>
+              확인
+            </ConfirmButton>
+          </Footer>
+        </Inner>
+      </Dimmer>
+    </Portal>
+  );
+};
 
 export default Dialog;
