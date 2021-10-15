96a5c99cfc771de61acd409587d559c85d09aca0
bigsaigon333 <likepeppermint@gmail.com>
2021-10-15 20:05:13 +0900

    feat: Dialog의 open/close 여부는 외부에서 제어한다

--- front-end/src/components/Dialog/Dialog.tsx
+++ front-end/src/components/Dialog/Dialog.tsx
@@ -1,52 +1,35 @@
-import { ReactNode, MouseEventHandler, MouseEvent, ComponentProps } from "react";
+import { ReactNode, MouseEventHandler, ComponentProps, HTMLAttributes } from "react";
 import Dimmer from "../Dimmer/Dimmer";
 import Portal from "../Portal/Portal";
 import { Inner, Body, Footer, CancelButton, ConfirmButton } from "./Dialog.styled";
 
-interface DialogProps {
+interface DialogProps extends HTMLAttributes<HTMLDialogElement> {
   dimmedColor?: ComponentProps<typeof Dimmer>["color"];
-  isOpen?: boolean;
-  close: () => void;
   onConfirm: MouseEventHandler<HTMLButtonElement>;
   onCancel?: MouseEventHandler<HTMLButtonElement>;
   children: ReactNode;
-  type: "alert" | "confirm";
   className?: string;
 }
 
 const Dialog = ({
   dimmedColor,
-  isOpen = true,
-  close,
   children,
   onConfirm,
   onCancel,
-  type,
   className,
-}: DialogProps) => {
-  const handleConfirm = (event: MouseEvent<HTMLButtonElement>) => {
-    onConfirm(event);
-    close();
-  };
-
-  const handleCancel = (event: MouseEvent<HTMLButtonElement>) => {
-    onCancel?.(event);
-    close();
-  };
-
-  return isOpen ? (
-    <Portal>
-      <Dimmer color={dimmedColor}>
-        <Inner className={className} aria-describedby="dialog-body">
-          <Body id="dialog-body">{children}</Body>
-          <Footer>
-            {type === "confirm" && <CancelButton onClick={handleCancel}>취소</CancelButton>}
-            <ConfirmButton onClick={handleConfirm}>확인</ConfirmButton>
-          </Footer>
-        </Inner>
-      </Dimmer>
-    </Portal>
-  ) : null;
-};
+  ...rest
+}: DialogProps) => (
+  <Portal>
+    <Dimmer color={dimmedColor}>
+      <Inner className={className} aria-describedby="dialog-body" {...rest}>
+        <Body id="dialog-body">{children}</Body>
+        <Footer>
+          {onCancel ? <CancelButton onClick={onCancel}>취소</CancelButton> : null}
+          <ConfirmButton onClick={onConfirm}>확인</ConfirmButton>
+        </Footer>
+      </Inner>
+    </Dimmer>
+  </Portal>
+);
 
 export default Dialog;
