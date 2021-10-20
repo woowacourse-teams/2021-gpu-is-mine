d36486e9fb8444535489e7b740610906913baa38
wannte <49307266+wannte@users.noreply.github.com>
2021-10-20 14:43:00 +0900

    feat: DB Replication 구현 (#426)
    * feat: replication 설정 spring 서버 반영
    
    * fix: db 설정 분리
    
    * feat: hikari prefix 설정
    
    * fix: DataSourceAutoConfiguration exclude
    
    * feat: logging 추가 및 dev를 replication db를 사용하도록 수정 (로깅 레벨 DEBUG)
    
    * chore: DataSourceConfig dev profile 추가
    
    * test: Propagation Required_new
    
    * Revert "test: Propagation Required_new"
    
    This reverts commit 19dc37162df84243beeaa926c0f9bebf314d1439.
    
    * refactor: add jpa.open-in-view: false
    
    * refactor: `@FlywayDataSource` annotation 사용하여 Flyway가 migration할 DataSource 명시적으로 지정
    
    Flyway의 스키마 변경이 있었을 때, Slave DB에 자동으로 반영이 되는지 확인 필요
    
    * test: flyway sync
    
    * Revert "test: flyway sync"
    
    This reverts commit f1d394896a92a66a0a653cf015e4f702eb50cbd4.
    
    * chore: dev-db 도커에서 동작하도록 분리

---
+++ back-end/src/main/java/mine/is/gpu/config/DataSourceConfig.java
@@ -0,0 +1,56 @@
+package mine.is.gpu.config;
+
+import static mine.is.gpu.config.ReplicationRoutingDataSource.DATASOURCE_KEY_MASTER;
+import static mine.is.gpu.config.ReplicationRoutingDataSource.DATASOURCE_KEY_SLAVE;
+
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.HashMap;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+@Profile("prod|was1|was2")
+@Configuration(proxyBeanMethods = false)
+public class DataSourceConfig {
+
+    @Bean
+    @FlywayDataSource
+    @ConfigurationProperties(prefix = "spring.datasource.hikari.master")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari.slave")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public DataSource routingDataSource(@Qualifier("masterDataSource") DataSource masterDataSource,
+                                        @Qualifier("slaveDataSource") DataSource slaveDataSource) {
+        ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
+
+        HashMap<Object, Object> sources = new HashMap<>();
+        sources.put(DATASOURCE_KEY_MASTER, masterDataSource);
+        sources.put(DATASOURCE_KEY_SLAVE, slaveDataSource);
+
+        routingDataSource.setTargetDataSources(sources);
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+
+        return routingDataSource;
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+}
