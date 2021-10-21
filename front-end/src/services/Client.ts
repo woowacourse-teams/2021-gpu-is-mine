fc44232d7731acb151aa16cc612145c01a0fc6dd
bigsaigon333 <likepeppermint@gmail.com>
2021-10-21 12:31:35 +0900

    refactor: Client 를 class에서 화살표 함수로 변경한다

--- front-end/src/services/Client.ts
+++ front-end/src/services/Client.ts
@@ -9,53 +9,51 @@ import type {
   MemberSignupRequest,
 } from "../types";
 
-const Client = class {
-  async postLogin({ email, password }: MemberLoginRequest) {
-    const {
-      data: { accessToken, expires: maxAge },
-    } = await httpClient.post<MemberLoginResponse>(API_ENDPOINT.MEMBER.LOGIN, {
-      email,
-      password,
-    });
+const postLogin = async ({ email, password }: MemberLoginRequest) => {
+  const {
+    data: { accessToken, expires: maxAge },
+  } = await httpClient.post<MemberLoginResponse>(API_ENDPOINT.MEMBER.LOGIN, {
+    email,
+    password,
+  });
 
-    const expires = new Date(Date.now() + maxAge);
+  const expires = new Date(Date.now() + maxAge);
 
-    return { accessToken, expires };
-  }
-
-  async fetchMyInfo({ accessToken, expires }: { accessToken: string; expires: Date }) {
-    validateExpires(expires);
-
-    const { data: myInfo } = await httpClient.get<MyInfoResponse>(API_ENDPOINT.MEMBER.ME, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    httpClient.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-
-    storage.set(STORAGE_KEY.ACCESS_TOKEN, accessToken);
-    storage.set(STORAGE_KEY.EXPIRES, expires);
-
-    return myInfo;
-  }
-
-  async postSignup({ email, password, name, labId, memberType }: MemberSignupRequest) {
-    await httpClient.post<never>(API_ENDPOINT.MEMBER.SIGNUP, {
-      email,
-      password,
-      name,
-      labId,
-      memberType,
-    });
-  }
-
-  // TODO: logout API 요청하기
-  async logout() {
-    storage.remove(STORAGE_KEY.ACCESS_TOKEN);
-    storage.remove(STORAGE_KEY.EXPIRES);
-  }
+  return { accessToken, expires };
 };
 
-const client = new Client();
+const fetchMyInfo = async ({ accessToken, expires }: { accessToken: string; expires: Date }) => {
+  validateExpires(expires);
+
+  const { data: myInfo } = await httpClient.get<MyInfoResponse>(API_ENDPOINT.MEMBER.ME, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  httpClient.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+
+  storage.set(STORAGE_KEY.ACCESS_TOKEN, accessToken);
+  storage.set(STORAGE_KEY.EXPIRES, expires);
+
+  return myInfo;
+};
+
+const postSignup = async ({ email, password, name, labId, memberType }: MemberSignupRequest) => {
+  await httpClient.post<never>(API_ENDPOINT.MEMBER.SIGNUP, {
+    email,
+    password,
+    name,
+    labId,
+    memberType,
+  });
+};
+
+// TODO: logout API 요청하기
+const logout = async () => {
+  storage.remove(STORAGE_KEY.ACCESS_TOKEN);
+  storage.remove(STORAGE_KEY.EXPIRES);
+};
+
+const client = { postLogin, fetchMyInfo, postSignup, logout };
 
 export default client;
