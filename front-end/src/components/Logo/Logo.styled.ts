f65d1cec95679cabcd8a6ac8efe1d4d327a29423
bigsaigon333 <likepeppermint@gmail.com>
2021-10-21 14:56:02 +0900

    feat: 로고의 'GPU 내껀데' 공백문자의 너비를 줄인다

--- front-end/src/components/Logo/Logo.styled.ts
+++ front-end/src/components/Logo/Logo.styled.ts
@@ -1,7 +1,13 @@
 import styled from "styled-components";
+import Text from "../Text/Text";
 
 export const StyledLogo = styled.h1`
   display: flex;
   align-items: center;
   column-gap: 0.75rem;
+`;
+
+export const StyledText = styled(Text)`
+  display: flex;
+  column-gap: 0.25rem;
 `;
