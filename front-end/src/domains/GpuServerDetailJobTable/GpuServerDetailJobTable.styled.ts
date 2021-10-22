83c6a7ef4cbd79e95fa24123ec6a372de388b9d6
Lee Soo Yeon <46412689+2SOOY@users.noreply.github.com>
2021-10-22 09:30:55 +0900

    feat: GPU 서버 및 JOB 상세 조회 페이지 스타일 개선 (#449)
    * feat: CardStyle 스타일 추가
    
    * feat: 상세 페이지 영역별 cardStyle 적용
    
    * feat: 현재 실행 Job 요약 정보 뷰 수정
    
    * feat: 현재 실행 중인 Job이 없는 경우 처리
    
    * style: log와 job table에 card 영역 fit-content 적용
    
    * style: 로그 컨테이너 영역 늘이기
    
    * feat: 불필요한 텍스트 제거
    
    Co-authored-by: Danny Kim <likepeppermint@gmail.com>
    
    * fix: jobId가 0일 때의 처리 추가
    
    * style: 로그 출력부 타이틀과 여백 늘리기
    
    * feat: Job 목록이 존재하지 않을 때 화면 처리
    
    * fix: 데이터가 없을 때의 페이지네이션 처리
    
    * feat: Job 요약 정보 관련 컴포넌트 분리 및 gpuServer, jobDetail 페이지 적용
    
    Co-authored-by: Danny Kim <likepeppermint@gmail.com>

--- front-end/src/domains/GpuServerDetailJobTable/GpuServerDetailJobTable.styled.ts
+++ front-end/src/domains/GpuServerDetailJobTable/GpuServerDetailJobTable.styled.ts
@@ -1,7 +1,15 @@
 import styled from "styled-components";
+import { cardStyle } from "../../styles";
 
 export const StyledGpuServerDetailJobTable = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 1rem;
+  height: 100%;
+
+  ${cardStyle}
+`;
+
+export const EmptyJobContainer = styled.div`
+  margin: auto;
 `;
---
+++ front-end/src/domains/JobDetailSummaryContent/JobDetailSummaryContent.tsx
@@ -0,0 +1,42 @@
+import { Text } from "../../components";
+import { SummaryList, Anchor } from "./JobDetailSummaryContent.styled";
+
+interface DetailProp {
+  title: string;
+  content: string | number;
+  isLink: boolean;
+}
+
+interface JobDetailSummaryProps {
+  title?: string;
+  className?: string;
+  detailList: DetailProp[];
+}
+
+const JobDetailSummaryContent = ({ detailList, ...rest }: JobDetailSummaryProps) => (
+  <SummaryList {...rest}>
+    {detailList.map(({ title, content, isLink }) =>
+      isLink ? (
+        <>
+          <Text as="dt" weight="bold">
+            {title}
+          </Text>
+          <Text as="dd">
+            <Anchor target="_blank" href={`https://hub.docker.com/r/${content}`} rel="noreferrer">
+              {content}
+            </Anchor>
+          </Text>
+        </>
+      ) : (
+        <>
+          <Text as="dt" weight="bold">
+            {title}
+          </Text>
+          <Text as="dd">{content}</Text>
+        </>
+      )
+    )}
+  </SummaryList>
+);
+
+export default JobDetailSummaryContent;
