d5c75d2c51d6af4b1c68f42c874472fa6a48bb52
2sooy <1401660@naver.com>
2021-10-24 15:28:06 +0900

    feat: job관련 API 함수 추가

---
+++ front-end/src/services/api/jobApiClient.ts
@@ -0,0 +1,69 @@
+/* eslint-disable class-methods-use-this */
+import { API_ENDPOINT } from "../../constants";
+import { httpClient } from "../../utils";
+import type {
+  JobDetailLogResponse,
+  JobRegisterRequest,
+  JobViewResponse,
+  JobViewResponses,
+  ParsedLogResponse,
+} from "../../types";
+
+type RequestJobById = {
+  [key in "labId" | "jobId"]: number;
+};
+
+const getJobAll = async ({ labId }: { labId: number }) => {
+  const { data } = await httpClient.get<JobViewResponses>(API_ENDPOINT.LABS(labId).JOBS);
+
+  return data.jobResponses;
+};
+
+const getJobById = async ({ labId, jobId }: RequestJobById) => {
+  const { data } = await httpClient.get<JobViewResponse>(
+    `${API_ENDPOINT.LABS(labId).JOBS}/${jobId}`
+  );
+
+  return data;
+};
+
+const postJob = async ({
+  labId,
+  name,
+  expectedTime,
+  gpuServerId,
+  metaData,
+}: { labId: number } & JobRegisterRequest) => {
+  const reqBody = {
+    name,
+    gpuServerId,
+    expectedTime,
+    metaData,
+  };
+
+  await httpClient.post(API_ENDPOINT.LABS(labId).JOBS, reqBody);
+};
+
+const putJobById = async ({ labId, jobId }: RequestJobById) => {
+  await httpClient.put(`${API_ENDPOINT.LABS(labId).JOBS}/${jobId}/cancel`);
+};
+
+const getJobLogById = async ({ labId, jobId }: RequestJobById) => {
+  const { data } = await httpClient.get<JobDetailLogResponse>(
+    `${API_ENDPOINT.LABS(labId).JOBS}/${jobId}/logs`
+  );
+
+  return data;
+};
+
+const getJobGraphById = async ({ labId, jobId }: RequestJobById) => {
+  const { data } = await httpClient.get<ParsedLogResponse>(
+    `${API_ENDPOINT.LABS(labId).JOBS}/${jobId}/logs-graph`
+  );
+
+  return data;
+};
+
+const client = { getJobAll, getJobById, postJob, putJobById, getJobLogById, getJobGraphById };
+
+export default client;
--- front-end/src/services/index.ts
+++ front-end/src/services/index.ts
@@ -1,3 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as authApiClient } from "./api/authApiClient";
+export { default as jobApiClient } from "./api/jobApiClient";
 export { default as storage } from "./Storage";
