f128f432cb1b6f15b7fbad16372debe5e673ce48
bigsaigon333 <likepeppermint@gmail.com>
2021-10-25 20:51:59 +0900

    refactor: useServerId를 useHistory로 이전한다

--- front-end/src/hooks/useHistory/useHistory.test.tsx
+++ front-end/src/hooks/useHistory/useHistory.test.tsx
@@ -1,5 +1,7 @@
+import type { ReactNode } from "react";
 import { renderHook, act } from "@testing-library/react-hooks/dom";
-import { pathList, useGoToPage, useMoveToPage, useParseParams } from "./useHistory";
+import { MemoryRouter, Route } from "react-router-dom";
+import { pathList, useGoToPage, useMoveToPage, useParseParams, useServerId } from "./useHistory";
 
 const mockGo = jest.fn();
 
@@ -67,5 +69,36 @@ describe("useParseParams", () => {
     const { result } = renderHook(() => useParseParams(key));
 
     expect(result.current).toBe(value);
+  });
+});
+
+const Wrapper = ({ path, children }: { path: string; children?: ReactNode }) => (
+  <MemoryRouter initialEntries={[path]}>
+    <Route exact strict path={["/:serverId", "*"]}>
+      {children}
+    </Route>
+  </MemoryRouter>
+);
+
+describe("useServerId", () => {
+  test("serverId Params가 path와 일치하는 경우, number로 변환하여 반환한다. ", () => {
+    const serverId = 35;
+    const { result } = renderHook(() => useServerId(), {
+      initialProps: { path: `/${serverId}` },
+      wrapper: Wrapper,
+    });
+
+    expect(result.current).toBe(serverId);
+  });
+
+  test("serverId Params가 path와 일치하지 않는 경우, 예외가 발생한다. ", () => {
+    ["", "/abc", "path/to/test"].forEach((path) => {
+      const { result } = renderHook(() => useServerId(), {
+        initialProps: { path },
+        wrapper: Wrapper,
+      });
+
+      expect(result.error).not.toBeUndefined();
+    });
   });
 });
