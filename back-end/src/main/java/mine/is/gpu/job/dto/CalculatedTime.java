6e38616002ffa6ed149accd6afe207890df0210a
ecsimsw <ecsimsw@gmail.com>
2021-10-26 17:35:49 +0900

    refactor : job 취소시 endedTime 값 기록

---
+++ back-end/src/main/java/mine/is/gpu/job/dto/CalculatedTime.java
@@ -0,0 +1,44 @@
+package mine.is.gpu.job.dto;
+
+import java.time.LocalDateTime;
+import mine.is.gpu.job.domain.Job;
+
+public class CalculatedTime {
+
+    private LocalDateTime createdTime;
+    private LocalDateTime startedTime;
+    private LocalDateTime expectedStartedTime;
+    private LocalDateTime completedTime;
+    private LocalDateTime expectedCompletedTime;
+
+    public CalculatedTime() {
+    }
+
+    public CalculatedTime(Job job) {
+        this.createdTime = job.getCreatedAt();
+        this.startedTime = job.getStartedTime();
+        this.completedTime = job.getCompletedTime();
+        this.expectedStartedTime = job.getExpectedStartedTime();
+        this.expectedCompletedTime = job.getExpectedCompletedTime();
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public LocalDateTime getStartedTime() {
+        return startedTime;
+    }
+
+    public LocalDateTime getExpectedStartedTime() {
+        return expectedStartedTime;
+    }
+
+    public LocalDateTime getEndedTime() {
+        return endedTime;
+    }
+
+    public LocalDateTime getExpectedCompletedTime() {
+        return expectedCompletedTime;
+    }
+}
