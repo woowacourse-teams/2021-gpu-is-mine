471103566462a62277f8589d4a3938117bbdb9eb
2sooy <1401660@naver.com>
2021-10-28 11:00:27 +0900

    feat: job 등록 폼 서버 목록 확인 관련 검증 로직 제거

--- front-end/src/domains/JobRegisterForm/JobRegisterForm.test.tsx
+++ front-end/src/domains/JobRegisterForm/JobRegisterForm.test.tsx
@@ -1,12 +1,9 @@
 import { render, screen, userEvent } from "../../__test__/test-utils";
 import JobRegisterForm from "./JobRegisterForm";
-import { gpuServersResponses } from "../../__fixtures__";
 
 describe("JobRegisterForm", () => {
   test("docker hub Image Input에 docker pull 을 입력하면 생략된다 ", async () => {
     render(<JobRegisterForm labId={1} />);
-
-    await screen.findAllByText(gpuServersResponses.gpuServers[0].serverName);
 
     const dockerHubImageInput = screen.getByRole("combobox", { name: "Docker Hub Image" });
 
