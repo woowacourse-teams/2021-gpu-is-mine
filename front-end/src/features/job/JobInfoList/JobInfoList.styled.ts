4afec1bad00e81e38dbe4fe00b6db34f70839b4a
김동희 <likepeppermint@gmail.com>
2021-10-29 10:07:39 +0900

    feat: 유저의 경우 토글 버튼으로 전체 Job을 조회할 수 있다 (#531)
    * feat: 유저의 경우 토글 버튼으로 전체 Job을 조회할 수 있다
    
    * feat: 유저가 등록한 Job이 나타나지 않는 경우 나의 Job 임을 명시한다
    
    * feat: 취소 권한이 없는 JOB에 대한 처리 추가
    
    * feat: throwError 대신 rejectWithValue를 통해 error.payload 사용
    
    Co-authored-by: 2sooy <1401660@naver.com>

--- front-end/src/features/job/JobInfoList/JobInfoList.styled.ts
+++ front-end/src/features/job/JobInfoList/JobInfoList.styled.ts
@@ -7,3 +7,19 @@ export const StyledJobInfoList = styled.ul`
   row-gap: 0.75rem;
   width: 100%;
 `;
+
+export const Label = styled.label`
+  align-self: flex-start;
+
+  display: flex;
+  align-items: center;
+  column-gap: 0.5rem;
+
+  margin-right: 2rem;
+  user-select: none;
+
+  input[type="checkbox"] {
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+`;
