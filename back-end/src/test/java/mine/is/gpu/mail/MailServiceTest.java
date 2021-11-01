6f4db8ec0e3cacc2ac6b1f065c152c8f6370cf99
Wannte <T.wannte@gmail.com>
2021-11-01 16:47:26 +0900

    chore: mailServiceTest disabled 처리

--- back-end/src/test/java/mine/is/gpu/mail/MailServiceTest.java
+++ back-end/src/test/java/mine/is/gpu/mail/MailServiceTest.java
@@ -3,12 +3,14 @@ package mine.is.gpu.mail;
 import mine.is.gpu.infra.MailService;
 import mine.is.gpu.infra.SendableJobData;
 import mine.is.gpu.job.domain.JobStatus;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@Disabled
 @ActiveProfiles("test")
 @SpringBootTest
 class MailServiceTest {
@@ -18,6 +20,6 @@ class MailServiceTest {
     @DisplayName("메일 보내기")
     @Test
     void mailTest() {
-        mailService.sendJobMail("ktw9695@naver.com", JobStatus.WAITING, new SendableJobData(1L, "잡 이름 샘플"));
+        mailService.sendJobMail("email@email.com", JobStatus.WAITING, new SendableJobData(1L, "잡 이름 샘플"));
     }
 }
