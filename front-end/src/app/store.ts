29dd6398c4b9a081ed37a73dcb6a88e804f2ef97
bigsaigon333 <likepeppermint@gmail.com>
2021-11-08 17:25:40 +0900

    feat: development 일때만 logger middleware를 추가한다

--- front-end/src/app/store.ts
+++ front-end/src/app/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { SLICE_NAME } from "../constants";
+import logger from "redux-logger";
+import { __IS_PRODUCTION__, SLICE_NAME } from "../constants";
 import authReducer from "../features/member/authSlice";
 import signupReducer from "../features/member/signupSlice";
 import gpuServerReducer from "../features/gpuServer/gpuServerSlice";
@@ -12,6 +13,9 @@ export const store = configureStore({
     [SLICE_NAME.GPU_SERVER]: gpuServerReducer,
     [SLICE_NAME.JOB]: jobReducer,
   },
+  middleware: __IS_PRODUCTION__
+    ? undefined
+    : (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
